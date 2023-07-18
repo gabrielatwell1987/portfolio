1ce27b60d96fe39f9952e4c66527769a95125cb9
gabrielatwell1987 <gabrielatwell@proton.me>
2023-07-18 13:23:55 -0700

    Resume2

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -4,23 +4,25 @@
 
 <h1>Actual things I've built</h1>
 
-<a href="https://gabe1.vercel.app" target="_blank">Web Dev Website</a>
-<a href="https://gabe2.vercel.app" target="_blank">Horizontal Scroller</a>
-<a href="https://gabe3.vercel.app" target="_blank">Random User</a>
-<a href="https://gabe4.vercel.app" target="_blank">Starbucks Clone</a>
-<a href="https://formapp-three.vercel.app" target="_blank">Form Application</a>
-<a href="https://kindler.vercel.app" target="_blank">CRUD-type Application</a>
-<a href="https://github-vert.vercel.app" target="_blank">Github API</a>
-<a href="https://appity.vercel.app" target="_blank">Code Snippet</a>
-<a href="https://pokedex.vercel.app" target="_blank">Pokedex</a>
-<a href="https://svelteflix-delta.vercel.app" target="_blank">Netflix Clone</a>
-<a href="https://blog-until-you-die.vercel.app" target="_blank">Blog Until You Die</a>
+<div class="links">
+    <a href="https://gabe1.vercel.app" target="_blank">Web Dev Website</a><br>
+    <a href="https://gabe2.vercel.app" target="_blank">Horizontal Scroller</a><br>
+    <a href="https://gabe3.vercel.app" target="_blank">Random User</a><br>
+    <a href="https://gabe4.vercel.app" target="_blank">Starbucks Clone</a><br>
+    <a href="https://formapp-three.vercel.app" target="_blank">Form Application</a><br>
+    <a href="https://kindler.vercel.app" target="_blank">CRUD-type Application</a><br>
+    <a href="https://github-vert.vercel.app" target="_blank">Github API</a><br>
+    <a href="https://appity.vercel.app" target="_blank">Code Snippet</a><br>
+    <a href="https://pokedex.vercel.app" target="_blank">Pokedex</a><br>
+    <a href="https://svelteflix-delta.vercel.app" target="_blank">Netflix Clone</a><br>
+    <a href="https://blog-until-you-die.vercel.app" target="_blank">Blog Until You Die</a>
+</div>
 
 <style>
     h1 {
         text-align: center;
         font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
-        font-size: 1.5rem;
+        font-size: 3.5rem;
         color: antiquewhite;
     }
     a {
@@ -32,5 +34,8 @@
     }
     a:hover {
         color: #f00;
+    }
+    .links {
+        text-align: center;
     }
 </style>
