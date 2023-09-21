3834463bafab5478e01d96c23a6eb10408a6cc65
gabrielatwell1987 <gabrielatwell@proton.me>
2023-09-21 14:53:37 -0700

    analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,47 +1,48 @@
 <script>
-    import NavBar from '$lib/NavBar.svelte';
-    import NavBar2 from '$lib/NavBar2.svelte';
-    import Discord from '$lib/Discord.svelte';
-    import Reddit from '$lib/Reddit.svelte';
+	import NavBar from '$lib/NavBar.svelte';
+	import NavBar2 from '$lib/NavBar2.svelte';
+	import Discord from '$lib/Discord.svelte';
+	import Reddit from '$lib/Reddit.svelte';
+	import Analytics from '../lib/Analytics.svelte';
 </script>
 
+<Analytics />
+
 <main class="desktop">
-    <NavBar />
-    <br>
-    <slot></slot>
+	<NavBar />
+	<br />
+	<slot />
 </main>
 
 <main class="mobile">
-    <NavBar2 />
-    <br>
-    <slot></slot>
+	<NavBar2 />
+	<br />
+	<slot />
 </main>
 
-
-
 <footer>
-    <a href="https://www.reddit.com/u/gatwell702">
-        <Reddit/>
-    </a>
-    <a href="https://www.discord.com">
-        <Discord/>
-    </a>
+	<a href="https://www.reddit.com/u/gatwell702">
+		<Reddit />
+	</a>
+	<a href="https://www.discord.com">
+		<Discord />
+	</a>
 </footer>
 
 <style>
-    a {
-        text-decoration: none;
-        color: lightskyblue;
-    }
-    a:hover {
-        color: blue;
-    }
-    .mobile {
-        display: none;
-    }
-    @media (max-width: 1250px) {
-        footer {
-            display: none;
-        }
-    }
+	a {
+		text-decoration: none;
+		color: lightskyblue;
+	}
+	a:hover {
+		color: blue;
+	}
+	.mobile {
+		display: none;
+	}
+	@media (max-width: 1250px) {
+		footer {
+			display: none;
+		}
+	}
 </style>
