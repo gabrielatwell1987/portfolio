4e806b94b6fe9632b3d5ccf0eec493f9fc66b3d2
gabrielatwell1987 <gabrielatwell@proton.me>
2023-10-19 15:46:55 -0700

    spacing

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -8,6 +8,7 @@
 		gsap.set('.title', { y: 75, scale: 0 });
 
 		let tl = gsap.timeline({ defaults: { duration: 1.5 } });
+
 		tl.to('.title', { scale: 3 }).to('.title', { scale: 1, duration: 2 });
 	});
 </script>
