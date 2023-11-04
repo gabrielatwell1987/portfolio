138172e06140943128a52d873177a9ef56115157
gabrielatwell1987 <gabrielatwell@proton.me>
2023-11-04 13:10:42 -0700

    font-family

--- src/routes/photos/+page.svelte
+++ src/routes/photos/+page.svelte
@@ -79,7 +79,7 @@
 		color: var(--cornflower);
 		margin: 0 0 2rem 0;
 		font-size: 2.5rem;
-		font-family: 'Courier New', Courier, monospace;
+		font-family: Arial, Helvetica, sans-serif;
 	}
 	section {
 		background-color: var(--lightblack);
