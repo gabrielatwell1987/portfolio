78dbe534a89dd89fde01ddfbedbedbdbcd77dfc9
gabrielatwell1987 <gabrielatwell@proton.me>
2023-12-10 12:51:51 -0800

    updated greensock text

--- src/routes/blog/greensock/+page.svelte
+++ src/routes/blog/greensock/+page.svelte
@@ -39,11 +39,12 @@
 	<h3 class="heading">How I used GSAP</h3>
 
 	<p>
-		In the project, you have to npm install gsap. In sveltekit, you have to put all of your GSAP
-		code in an onMount function that you have to import. This is because onMount is displayed once
-		on initial load. If you were to use a regular script tag, it would not work. All of the designs
-		that are animated on this website are all from GSAP. I tend to think that these animations make
-		the website look a lot better than a static website. GSAP is a great tool to use for any web
+		In the project, you have to npm install gsap (if you have Node.js installed) or you can use the
+		CDN (content delivery network). In sveltekit, you have to put all of your GSAP code in an
+		onMount function that you have to import. This is because onMount is displayed once on initial
+		load. If you were to use a regular script tag, it would not work. All of the designs that are
+		animated on this website are all from GSAP. I tend to think that these animations make the
+		website look a lot better than a static website. GSAP is a great tool to use for any web
 		developer. I highly recommend it!
 	</p>
 
