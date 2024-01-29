18b7ecfc7573a4593e10487ebebcacfa90f8d36b
gabrielatwell1987 <gabrielatwell@proton.me>
2024-01-29 15:58:21 -0800

    hero image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -89,6 +89,7 @@
 			height: 100vh;
 			position: relative;
 			/* background: url('logos/fire.png') no-repeat center center/cover fixed; */
+			background: url('logos/hero-bg-img.jpg') no-repeat center center/cover fixed;
 		}
 
 		h3 {
