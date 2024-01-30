8f02d1e203246fe372c030793d274cfa08076ef4
gabrielatwell1987 <gabrielatwell@proton.me>
2024-01-29 16:17:29 -0800

    hero

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -88,7 +88,7 @@
 			font-size: 1.75rem;
 			height: 100vh;
 			position: relative;
-			background: url('logos/bg3.jpg') no-repeat center center/cover fixed;
+			/* background: url('logos/bg3.jpg') no-repeat center center/cover fixed; */
 		}
 
 		h3 {
