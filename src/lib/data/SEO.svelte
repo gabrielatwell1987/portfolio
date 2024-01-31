9e43da11850993dcd7a62d66019bd007e7272fc8
gabrielatwell1987 <gabrielatwell@proton.me>
2024-01-31 15:17:14 -0800

    seo

--- src/lib/data/SEO.svelte
+++ src/lib/data/SEO.svelte
@@ -1,15 +1,17 @@
 <script>
 	import { page } from '$app/stores';
 
-	export let title = '';
-	export let description = '';
+	export let title;
+	export let description;
 
 	$: url = $page.url.href;
 </script>
 
 <svelte:head>
 	<title>{title}</title>
+
 	<meta name="description" content={description} />
+
 	<script type="application/ld+json">
 		{
 			"@context": "http://schema.org",
