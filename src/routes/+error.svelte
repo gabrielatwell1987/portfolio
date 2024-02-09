4900470b7d8ec2de1ed19de7adf91bf6f6e8725a
gabrielatwell1987 <gabrielatwell@proton.me>
2024-02-08 16:12:22 -0800

    created an error.svelte page for the whole site

---
+++ src/routes/+error.svelte
@@ -0,0 +1,31 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<main>
+	<h1>{$page.status}</h1>
+
+	<p>{$page.error.message}</p>
+</main>
+
+<style>
+	h1 {
+		font-size: 5rem;
+		margin: 5rem auto;
+		text-align: center;
+		color: var(--sky);
+		font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+		letter-spacing: 5px;
+	}
+
+	p {
+		font-size: 2rem;
+		letter-spacing: 3px;
+		text-align: left;
+		border: 3px solid var(--smoke);
+		border-radius: 12px;
+		padding: 1rem 2.5rem;
+		color: var(--white);
+		font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+	}
+</style>
