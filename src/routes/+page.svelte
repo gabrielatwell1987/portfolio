7d238f7efa5a63bb8babd16f92ea2eeacbcb1389
gabrielatwell1987 <gabrielatwell@proton.me>
2024-02-13 16:38:11 -0800

    hero section image alt text update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,7 +14,7 @@
 	<img
 		class="hero-image"
 		src="https://images.pexels.com/photos/196644/pexels-photo-196644.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-		alt="Code"
+		alt="Text within a hero section."
 	/>
 
 	<section class="hero-text">
