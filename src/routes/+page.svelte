3810a82456fd4eafb194555cc4f132fcdc87e253
gabrielatwell1987 <gabrielatwell@proton.me>
2024-02-13 16:39:57 -0800

    hero section image alt text update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,7 +14,7 @@
 	<img
 		class="hero-image"
 		src="https://images.pexels.com/photos/196644/pexels-photo-196644.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-		alt="Text within a hero section."
+		alt="Wireframing and design of a website"
 	/>
 
 	<section class="hero-text">
