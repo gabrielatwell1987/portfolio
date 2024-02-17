a50ce5becc4cb2ab499592569f1c9b6eb5b7e8a1
gabrielatwell1987 <gabrielatwell@proton.me>
2024-02-17 13:42:16 -0800

    i created a hero section with three.js and linked it to my about page

---
+++ src/lib/components/ThreeCanvas.svelte
@@ -0,0 +1,168 @@
+<script>
+	import { onMount } from 'svelte';
+	import * as THREE from 'three';
+	// import * as dat from "dat.gui";
+
+	onMount(() => {
+		// Texture Loader
+		const textureLoader = new THREE.TextureLoader();
+		const cross = textureLoader.load('/gltf/cross.png');
+
+		// // Debug
+		// const gui = new dat.GUI();
+
+		// Canvas
+		const canvas = document.querySelector('canvas.webgl');
+
+		// Scene
+		const scene = new THREE.Scene();
+
+		// Objects
+		const geometry = new THREE.CircleGeometry(1, 50);
+
+		const particlesGeometry = new THREE.BufferGeometry();
+		const particlesCount = 5000;
+		const positionArray = new Float32Array(particlesCount * 3);
+
+		for (let i = 0; i < particlesCount * 3; i++) {
+			positionArray[i] = (Math.random() - 0.5) * 5;
+		}
+
+		particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positionArray, 3));
+
+		// Materials
+
+		const material = new THREE.PointsMaterial({
+			size: 0.009
+		});
+
+		const particlesMaterial = new THREE.PointsMaterial({
+			size: 0.005,
+			map: cross,
+			transparent: true,
+			color: 'hsla(187, 64%, 73%, 0.75)',
+			blending: THREE.AdditiveBlending
+		});
+
+		// Mesh
+		const sphere = new THREE.Points(geometry, material);
+		const particlesMesh = new THREE.Points(particlesGeometry, particlesMaterial);
+		scene.add(sphere, particlesMesh);
+
+		// Lights
+
+		const pointLight = new THREE.PointLight(0xffffff, 0.1);
+		pointLight.position.x = 2;
+		pointLight.position.y = 3;
+		pointLight.position.z = 4;
+		scene.add(pointLight);
+
+		/**
+		 * Sizes
+		 */
+		const sizes = {
+			width: window.innerWidth,
+			height: window.innerHeight
+		};
+
+		window.addEventListener('resize', () => {
+			// Update sizes
+			sizes.width = window.innerWidth;
+			sizes.height = window.innerHeight;
+
+			// Update camera
+			camera.aspect = sizes.width / sizes.height;
+			camera.updateProjectionMatrix();
+
+			// Update renderer
+			renderer.setSize(sizes.width, sizes.height);
+			renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+		});
+
+		/**
+		 * Camera
+		 */
+		// Base camera
+		const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+		camera.position.x = 0;
+		camera.position.y = 0;
+		camera.position.z = 2;
+		scene.add(camera);
+
+		// Controls
+		// const controls = new OrbitControls(camera, canvas)
+		// controls.enableDamping = true
+
+		/**
+		 * Renderer
+		 */
+		const renderer = new THREE.WebGLRenderer({
+			canvas: canvas,
+			alpha: true,
+			antialias: true
+		});
+		renderer.setSize(sizes.width, sizes.height);
+		renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+		// Mouse
+		document.addEventListener('mousemove', animateParticles);
+
+		let mouseX = 0;
+		let mouseY = 0;
+
+		function animateParticles(event) {
+			mouseX = event.clientX;
+			mouseY = event.clientY;
+		}
+
+		/**
+		 * Animate
+		 */
+
+		const clock = new THREE.Clock();
+
+		const tick = () => {
+			const elapsedTime = clock.getElapsedTime();
+
+			// Update objects
+			sphere.rotation.y = 0.5 * elapsedTime;
+			particlesMesh.rotation.y = -0.1 * elapsedTime;
+
+			if (mouseX > 0) {
+				particlesMesh.rotation.x = -mouseY * (elapsedTime * 0.00008);
+				particlesMesh.rotation.y = -mouseX * (elapsedTime * 0.00008);
+			}
+
+			// Update Orbital Controls
+			// controls.update()
+
+			// Render
+			renderer.render(scene, camera);
+
+			// Call tick again on the next frame
+			window.requestAnimationFrame(tick);
+		};
+
+		tick();
+	});
+</script>
+
+<canvas class="webgl" />
+
+<h1>Atwell Design</h1>
+
+<style>
+	h1 {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		color: whitesmoke;
+		font-size: 3.5rem;
+		font-weight: 900;
+		font-family: var(--major);
+		text-align: center;
+		line-height: 1.2;
+		letter-spacing: 3px;
+	}
+</style>
---
+++ src/routes/hero/+page.svelte
@@ -0,0 +1,113 @@
+<script>
+	import ThreeCanvas from '$lib/components/ThreeCanvas.svelte';
+</script>
+
+<ThreeCanvas />
+
+<section class="hero-text">
+	<!-- hero section text and links -->
+	<h3>Frontend Crafted Web Experiences</h3>
+
+	<h4 class="summary">
+		I am a frontend developer who loves to create beautiful and functional websites. This portfolio
+		is a website that I built from scratch to showcase some projects that I created. I look forward
+		to hearing from you so we can discuss your goals for your online needs!
+	</h4>
+
+	<a class="btn" href="/projects"><button aria-label="projects">Projects</button></a>
+
+	<h4 class="summary italic">Some projects I have created.</h4>
+</section>
+
+<style>
+	.hero-text {
+		position: absolute;
+		top: 120%;
+		left: 50%;
+		transform: translate(-50%, -120%);
+		width: 100%;
+	}
+
+	h3 {
+		margin: 2rem auto;
+		margin-top: 10%;
+		color: var(--sky);
+		font-size: 2rem;
+		font-weight: 900;
+		font-family: var(--montserrat);
+		letter-spacing: 2px;
+	}
+
+	h4 {
+		color: var(--white);
+		font-size: 1rem;
+		font-weight: 400;
+		letter-spacing: 0.5px;
+		font-family: var(--montserrat);
+		width: 50%;
+		margin: 0 auto;
+	}
+
+	button {
+		display: block;
+		background-color: transparent;
+		color: var(--white);
+		font-family: var(--montserrat);
+		width: 10%;
+		height: 10%;
+		outline: none;
+		border: 1px solid var(--white);
+		margin: 1rem auto;
+		border-radius: 10px;
+		font-size: 1.2rem;
+		font-weight: 900;
+		border-radius: 5rem;
+		filter: drop-shadow(0 0 0.75rem var(--smoke));
+		letter-spacing: 1px;
+		transition: 750ms;
+		outline: 3px solid currentColor;
+		outline-offset: -7px;
+		margin-top: 2rem;
+		margin-bottom: 0.25rem;
+		padding: 1rem 1.75rem;
+	}
+
+	button:hover,
+	button:focus {
+		border: 1px solid var(--black);
+		background-color: var(--white);
+		color: var(--black);
+		outline-offset: 0px;
+		animation: wiggle 0.5s ease-in-out infinite;
+	}
+
+	.summary {
+		margin: 0 auto;
+		font-size: 1rem;
+		font-weight: 500;
+		width: 75%;
+	}
+
+	.italic {
+		font-style: italic;
+		font-family: var(--montserrat);
+		font-weight: 200;
+		font-size: 0.8rem;
+		color: var(--white);
+	}
+
+	@keyframes wiggle {
+		0% {
+			rotate: 0deg;
+			translate: 0 0;
+		}
+		25% {
+			rotate: -2deg;
+			translate: -5px 0;
+		}
+		80% {
+			rotate: 2deg;
+			translate: 5px 0;
+		}
+	}
+</style>
