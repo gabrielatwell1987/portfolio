bf87ed3b367406322e8aca1f0c7f6a36c0c51cf2
gabrielatwell1987 <gabrielatwell@proton.me>
2024-02-17 13:54:54 -0800

    removed lenis completely from site

--- src/routes/about/gallery/+page.svelte
+++ src/routes/about/gallery/+page.svelte
@@ -1,20 +1,5 @@
 <script>
 	import Gallery from '$lib/components/Gallery.svelte';
-	import Lenis from '@studio-freight/lenis';
-
-	// lenis
-	const lenis = new Lenis();
-
-	lenis.on('scroll', (e) => {
-		console.log(e);
-	});
-
-	function raf(time) {
-		lenis.raf(time);
-		requestAnimationFrame(raf);
-	}
-
-	requestAnimationFrame(raf);
 </script>
 
 <!-- Photo gallery -->
