547331f73e3228aaf9a94708df0d010898c8c8fe
gabrielatwell1987 <gabrielatwell@proton.me>
2024-04-05 13:02:42 -0700

    i added a back button on the posts blog page

--- src/routes/blog/posts/+page.svelte
+++ src/routes/blog/posts/+page.svelte
@@ -15,6 +15,9 @@
 
 	<h1 class="title">GSAP</h1>
 	<GSAP />
+
+	<!-- back button -->
+	<a class="blog-btn-link" href="/blog"><button class="blog-btn" aria-label="blog">Back</button></a>
 </section>
 
 <style>
@@ -31,5 +34,76 @@
 	}
 	section {
 		background: url('/logos/dead-flower.png') no-repeat center center/cover fixed;
+	}
+
+	@media (min-width: 200px) {
+		.blog-btn {
+			background-color: transparent;
+			color: var(--white);
+			border: 1px solid var(--white);
+			margin: 0 auto;
+			border-radius: 8px;
+			cursor: pointer;
+			font-size: 1.2rem;
+			letter-spacing: 1px;
+			width: 50%;
+			font-family: var(--lexend);
+			border-radius: 0.75rem;
+			filter: drop-shadow(0 0 0.75rem var(--smoke));
+			transition: 750ms;
+			outline: 3px solid currentColor;
+			outline-offset: -7px;
+			mix-blend-mode: difference;
+			padding: 1rem 1.75rem;
+			margin-top: 10%;
+			margin-bottom: 2%;
+			user-select: none;
+		}
+
+		.blog-btn:hover,
+		.blog-btn:focus {
+			opacity: 0.8;
+			outline-offset: 0px;
+			animation: wiggle 0.5s ease-in-out infinite;
+		}
+
+		@keyframes wiggle {
+			0% {
+				rotate: 0deg;
+				translate: 0 0;
+			}
+			25% {
+				rotate: -2deg;
+				translate: -5px 0;
+			}
+			80% {
+				rotate: 2deg;
+				translate: 5px 0;
+			}
+		}
+	}
+
+	@media (min-width: 500px) {
+		.blog-btn {
+			width: 35%;
+		}
+	}
+
+	@media (min-width: 720px) {
+		.blog-btn {
+			width: 30%;
+		}
+	}
+
+	@media (min-width: 990px) {
+		.blog-btn {
+			width: 20%;
+		}
+	}
+
+	@media (min-width: 1200px) {
+		.blog-btn {
+			width: 15%;
+		}
 	}
 </style>
