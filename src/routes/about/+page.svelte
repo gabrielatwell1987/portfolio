30a6d54504d1be14b9653010f6faf1bd17f4c27f
gabrielatwell1987 <gabrielatwell@proton.me>
2024-04-23 13:11:26 -0700

    test

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -8,7 +8,7 @@
 	import LinkList from '$lib/components/LinkList.svelte';
 	import Grid from '$lib/components/Grid.svelte';
 	import Skills from '$lib/components/Skills.svelte';
-	// import Image from '$lib/components/Image.svelte';
+	import Image from '$lib/components/Image.svelte';
 
 	// animations
 	onMount(() => {
@@ -36,13 +36,12 @@
 <main in:blur={{ delay: 350, duration: 1500, easing: quintOut, amount: '1rem' }} aria-label="bio">
 	<section title="a white flower background">
 		<article>
-			<div class="atwell">
-				<img src="/logos/atweezy.png" alt="An logo that says Gabriel Atwell that you can move." />
-				<!-- <Image
-					picture="/logos/atweezy.png"
-					desc="An logo that says Gabriel Atwell that you can move."
-				/> -->
-			</div>
+			<img
+				class="atwell"
+				src="logos/atweezy.png"
+				alt="An logo that says Gabriel Atwell that you can move."
+				width="500"
+			/>
 
 			<div class="bio" aria-label="bio">
 				<h2 class="main-title">About me</h2>
