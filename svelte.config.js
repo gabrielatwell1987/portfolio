d2974e5282542323f89ec42635ff707e74d946b0
gabrielatwell1987 <gabrielatwell@proton.me>
2024-04-27 15:28:26 -0700

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-// import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-vercel';
 
 import { mdsvex } from 'mdsvex';
 
