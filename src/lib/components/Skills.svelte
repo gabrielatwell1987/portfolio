b2f97614e2eca15884930ec42bf946a298efdc1a
gabrielatwell1987 <gabrielatwell@proton.me>
2024-05-12 13:41:21 -0700

    i added a skills folder with SVGs to all my skills, and updated the Skills component with those SVGs instead of just text

--- src/lib/components/Skills.svelte
+++ src/lib/components/Skills.svelte
@@ -1,19 +1,82 @@
 <script>
+	import Image from './Image.svelte';
+
 	export let title;
 </script>
 
 <section class="border" aria-label="skills">
 	<h3 class="skills-title">{title}</h3>
+
 	<ul class="skills">
-		<a href="https://developer.mozilla.org/en-US/docs/Learn/HTML"><li>HTML</li></a>
-		<a href="https://developer.mozilla.org/en-US/docs/Learn/CSS"><li>CSS</li></a>
-		<a href="https://developer.mozilla.org/en-US/docs/Learn/JavaScript"><li>JavaScript</li></a>
-		<a href="https://kit.svelte.dev/"><li>Sveltekit</li></a>
-		<a href="https://gsap.com"><li>Greensock</li></a>
-		<a href="https://nodejs.org"><li>Node.js</li></a>
-		<a href="https://github.com"><li>Git/GitHub</li></a>
-		<a href="https://sass-lang.com"><li>SCSS</li></a>
-		<a href="https://tailwindcss.com"><li>Tailwind CSS</li></a>
+		<a href="https://developer.mozilla.org/en-US/docs/Learn/HTML">
+			<li>
+				<div class="icons"><Image src="skills/html.svg" alt="html" /></div>
+			</li>
+		</a>
+
+		<a href="https://developer.mozilla.org/en-US/docs/Learn/CSS">
+			<li>
+				<div class="icons">
+					<Image src="skills/css.svg" alt="css" />
+				</div>
+			</li>
+		</a>
+
+		<a href="https://developer.mozilla.org/en-US/docs/Learn/JavaScript">
+			<li>
+				<div class="icons">
+					<Image src="skills/js.svg" alt="javascript" />
+				</div>
+			</li>
+		</a>
+
+		<a href="https://kit.svelte.dev/">
+			<li>
+				<div class="icons">
+					<Image src="skills/svelte.svg" alt="sveltekit" />
+				</div>
+			</li>
+		</a>
+
+		<a href="https://gsap.com">
+			<li>
+				<div class="icons">
+					<Image src="skills/greensock.svg" alt="greensock" />
+				</div>
+			</li>
+		</a>
+
+		<a href="https://nodejs.org">
+			<li>
+				<div class="icons">
+					<Image src="skills/node.svg" alt="node.js" />
+				</div>
+			</li>
+		</a>
+
+		<a href="https://github.com">
+			<li>
+				<div class="icons">
+					<Image src="skills/github.svg" alt="github" />
+				</div>
+			</li>
+		</a>
+
+		<a href="https://sass-lang.com">
+			<li>
+				<div class="icons">
+					<Image src="skills/scss.svg" alt="scss" />
+				</div>
+			</li>
+		</a>
+
+		<a href="https://tailwindcss.com">
+			<li>
+				<div class="icons">
+					<Image src="skills/tailwind.svg" alt="tailwind css" />
+				</div>
+			</li>
+		</a>
 	</ul>
 </section>
 
@@ -38,8 +101,9 @@
 		}
 
 		.skills {
-			/* display: flex;
-			flex-direction: column; */
+			display: flex;
+			flex-direction: column;
+			align-items: center;
 			gap: 1rem;
 			display: grid;
 			grid-template-columns: repeat(3, 1fr);
@@ -47,15 +111,10 @@
 
 		.skills li {
 			list-style: none;
-			margin: 0 -94%;
+			margin: 0 -92.5%;
 			padding: 0.5rem 0.75rem;
 			width: 100%;
 			font-size: 1rem;
-			font-weight: 400;
-			font-family: var(--montserrat);
-			letter-spacing: 1px;
-			background-color: var(--lightblack);
-			color: var(--white);
 			border-radius: 12px;
 			text-align: center;
 			mix-blend-mode: difference;
@@ -70,6 +129,12 @@
 			text-align: center;
 			font-family: var(--anta);
 			mix-blend-mode: hard-light;
+			letter-spacing: 7px;
+			margin-right: 1rem;
+		}
+
+		.icons {
+			width: 5rem;
 		}
 	}
 </style>
