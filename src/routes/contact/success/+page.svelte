b60d1d84affea638b3415552b4d27145ad35e7eb
gabrielatwell1987 <gabrielatwell@proton.me>
2024-05-18 12:34:19 -0700

    added a success page for contacting me

---
+++ src/routes/contact/success/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+	import Image from '$lib/components/Image.svelte';
+</script>
+
+<section>
+	<Image src="/logos/success.png" alt="Atwell" />
+
+	<h1>Success!</h1>
+
+	<p><i>Your message has been sent.</i></p>
+	<p><b>Thank you for reaching out.</b></p>
+</section>
+
+<style>
+	section {
+		text-align: center;
+		border: 1px solid var(--white);
+		border-radius: 10px;
+		padding: 2rem;
+		margin-top: 5%;
+		width: 50%;
+		margin-inline: auto;
+	}
+
+	h1 {
+		font-size: 2.5rem;
+		margin: 0.5em 0;
+	}
+
+	p {
+		font-size: 1.5rem;
+		margin: 0.5em 0;
+	}
+</style>
