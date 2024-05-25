bd12ca51e39a99a1c146deafa664e1f87aaf6ffc
gabrielatwell1987 <gabrielatwell@proton.me>
2024-05-25 13:21:52 -0700

    i created a firgure component that has a figcaption

---
+++ src/lib/components/Figure.svelte
@@ -0,0 +1,46 @@
+<script>
+	export let src;
+	export let alt;
+	export let text;
+</script>
+
+<figure>
+	<img {src} {alt} />
+
+	<figcaption><p>{text}</p></figcaption>
+</figure>
+
+<style>
+	figure {
+		margin: 0;
+		padding: 0;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		position: relative;
+	}
+
+	img {
+		border-radius: 2.5%;
+		max-width: 100%;
+	}
+
+	figcaption {
+		font-size: 1.25rem;
+		color: var(--gray);
+		position: absolute;
+		bottom: 0;
+		left: 49%;
+		transform: translate(-49%, 0);
+		color: antiquewhite;
+		backdrop-filter: blur(5px);
+		padding: 0.5rem;
+		border-radius: 0.5rem;
+	}
+
+	p {
+		margin: 0;
+		padding: 0;
+		text-align: center;
+	}
+</style>
