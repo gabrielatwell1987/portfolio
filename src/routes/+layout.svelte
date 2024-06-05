6dd68c7fe8fdda073c554bf007eb1942828215e5
gabrielatwell1987 <gabrielatwell@proton.me>
2024-06-05 12:46:41 -0700

    added SEO meta tags of layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,12 +8,13 @@
 <svelte:head>
 	<!-- website theme cdn -->
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css" />
-
 	<!-- font awesome cdn -->
 	<link
 		rel="stylesheet"
 		href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
 	/>
+	<meta name="description" content="Gabriel Atwell's Portfolio" />
+	<meta name="keywords" content="Gabriel Atwell's Portfolio" />
 </svelte:head>
 
 <!-- data from SEO and analytics -->
