15b32f6e8d1f39c627b4041c7c40c4610e6f5756
gabrielatwell1987 <gabrielatwell@proton.me>
2024-06-11 11:57:33 -0700

    .send button width on contact form component

--- src/lib/components/ContactForm.svelte
+++ src/lib/components/ContactForm.svelte
@@ -158,7 +158,7 @@
 			margin-inline: auto;
 			cursor: pointer;
 			letter-spacing: 1px;
-			width: 40%;
+			width: 45%;
 			border-radius: 0.75rem;
 			filter: drop-shadow(0 0 0.75rem var(--smoke));
 			transition: 750ms;
