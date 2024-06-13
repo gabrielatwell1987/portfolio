362e0e4e1d6c3f4543f83dfc495d4edfb414d0c0
gabrielatwell1987 <gabrielatwell@proton.me>
2024-06-13 11:52:59 -0700

    edited the Figure component on the projects page

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -148,7 +148,7 @@
 	<!-- logo -->
 	<div class="logos designs">
 		<!-- <img class="designs" src="logos/designs.png" alt="A logo that says Atwell Designs" /> -->
-		<Figure src="logos/designs.png" alt="A logo that says Atwell Designs" text="est. 2023" />
+		<Figure src="logos/designs.png" alt="A logo that says Atwell Designs" text="EST. 2023" />
 	</div>
 </section>
 
