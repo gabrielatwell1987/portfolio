bc4ce38d964b34657ec465d71e845ddaa8109e68
gabrielatwell1987 <gabrielatwell@proton.me>
2024-06-14 12:22:00 -0700

    test

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-
-	server: {
-		host: '192.168.0.83',
-		port: 5273 // Optional: specify the port
-	}
+	plugins: [sveltekit()]
 });
