237542f4ac5259c79bbf9ff1baa7b14df98eac7f
gabrielatwell1987 <gabrielatwell@proton.me>
2024-06-17 14:33:09 -0700

    testing the ACII component

--- src/lib/components/ACII.svelte
+++ src/lib/components/ACII.svelte
@@ -1,35 +1,36 @@
 <script></script>
 
-<pre>                                                                                            
-    <span style="color: #005F00;">                  __~gBBBBBBBBgg__              
-                  _gBBBBBBBBBBBBBBBBBBBg_           
-               _gBBBBBBBBBBBBBBBBBBBBBBBBB~         
-              gBBBBBBBBBBBBBBBBBBBBBBBBBBBBBg       
-             BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB1     
-            BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBr    
-           BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBF    
-           &quot;BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB     
-           :BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBE     
-            gBBBBBB@D9ABBBBBBBBBBBBBWB@BBBBBBB|     
-            mBBD    `   ``$BBBBBBW     !| TBBB}     
-            BBW            ]BBBE        &quot;  [BB%     
-            \BB            IBB0Q           [BB/     
-             ]B_          sB   [g         _BBB!     
-            ;BBBA_      _B&quot;     $Bg_    _ABBBB!     
-              BBBBBBBBBBBP      `BBBBBBBBBBBBg      
-              `BBBB@BBBBBB       BBBBBB@BBBW        
-               ]]&apos;   BBBBg   `   BBBBB   &quot;  :       
-                     BBBBBWBgBg@BBBBBB              
-                     %BBBBBBBBBBBBBBBB              
-                      BBBB Q@WBBF7] B               
-                      g  B  ] BBZ * .               
-                      @  &apos;    ZBN                   
-                              * &apos;                   
-                              [                     
-                              &quot;                     
-                              &apos;                     
-                                                    
-    </span>
+<pre>                                                
+                                                
+  <span style="color: #005F00;">                  __~gBBBBBBBBgg__              
+                _gBBBBBBBBBBBBBBBBBBBg_           
+             _gBBBBBBBBBBBBBBBBBBBBBBBBB~         
+            gBBBBBBBBBBBBBBBBBBBBBBBBBBBBBg       
+           BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB1     
+          BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBr    
+         BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBF    
+         &quot;BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB     
+         :BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBE     
+          gBBBBBB@D9ABBBBBBBBBBBBBWB@BBBBBBB|     
+          mBBD    `   ``$BBBBBBW     !| TBBB}     
+          BBW            ]BBBE        &quot;  [BB%     
+          \BB            IBB0Q           [BB/     
+           ]B_          sB   [g         _BBB!     
+          ;BBBA_      _B&quot;     $Bg_    _ABBBB!     
+            BBBBBBBBBBBP      `BBBBBBBBBBBBg      
+            `BBBB@BBBBBB       BBBBBB@BBBW        
+             ]]&apos;   BBBBg   `   BBBBB   &quot;  :       
+                   BBBBBWBgBg@BBBBBB              
+                   %BBBBBBBBBBBBBBBB              
+                    BBBB Q@WBBF7] B               
+                    g  B  ] BBZ * .               
+                    @  &apos;    ZBN                   
+                            * &apos;                   
+                            [                     
+                            &quot;                     
+                            &apos;                     
+                                                  
+  </span>
 </pre>
 
 <style>
@@ -44,8 +45,7 @@
 		line-height: 1.2;
 		margin: 10px auto;
 		background: transparent;
-		width: 30%;
-		overflow: hidden;
+		text-align: center;
 	}
 
 	@media (min-width: 300px) {
