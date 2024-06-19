adffb8b0f5f78be40b78563e4a81f5b7523aabe4
gabrielatwell1987 <gabrielatwell@proton.me>
2024-06-19 15:20:24 -0700

    updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Hero from '$lib/components/Hero.svelte';
+	import ThemeToggle from '$lib/components/ThemeToggle.svelte';
 </script>
 
 <svelte:head>
@@ -9,3 +10,4 @@
 </svelte:head>
 
 <Hero />
+<ThemeToggle />
