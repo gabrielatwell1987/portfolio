96f9994501571cff81241709cf6d0d488a2534bc
gabrielatwell1987 <gabrielatwell@proton.me>
2024-06-28 15:40:47 -0700

    testing logo on main page

--- src/lib/components/HeroLogo.svelte
+++ src/lib/components/HeroLogo.svelte
@@ -1,58 +1,71 @@
 <script>
 	import { onMount } from 'svelte';
 	import { gsap } from 'gsap';
-	import Image from '$lib/components/Image.svelte';
+	// import Image from '$lib/components/Image.svelte';
+
+	export let title;
 
 	onMount(() => {
-		gsap.set('.hero-logo', { autoAlpha: 0 });
+		gsap.set('.hero-logo', { autoAlpha: 0, y: -100 });
 
 		gsap.to('.hero-logo', {
 			duration: 3,
 			autoAlpha: 1,
-			y: -100,
-			ease: 'sin.out',
-			delay: 0.75
+			y: 0,
+			ease: 'back.out(4)',
+			delay: 0.75,
+			letterSpacing: '15px'
 		});
 	});
 </script>
 
 <main>
 	<div class="hero-logo">
-		<Image src="/logos/mono-atwell.png" alt="Gabriel Atwell's Logo" />
+		<!-- <Image src="/logos/mono-atwell.png" alt="Gabriel Atwell's Logo" /> -->
+		<h1>{title}</h1>
 	</div>
 </main>
 
 <style>
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
+
 	main {
 		display: flex;
 		justify-content: center;
 		align-items: center;
-		height: 50vh;
-		margin-top: 15%;
+		height: 30vh;
+		margin-top: 10%;
+	}
+
+	h1 {
+		font-size: clamp(1.55rem, 10vw, 10rem);
+		letter-spacing: 5px;
+		font-family: var(--montserrat);
+		color: var(--dark-gray);
+		text-transform: uppercase;
+		text-align: center;
+		margin-bottom: 22%;
 	}
 
 	@media (max-width: 300px) {
 		main {
-			margin-top: 17rem;
-			margin-bottom: 10%;
+			height: 10vh;
 		}
 	}
 
 	@media (max-width: 720px) {
 		main {
-			margin-top: 15%;
-			margin-bottom: 10%;
-		}
-
-		.hero-logo {
-			width: 100%;
+			height: 20vh;
 		}
 	}
 
-	/* @media (max-width: 990px) {
-		.hero-logo {
-			padding-top: -25%;
-			margin-bottom: -5%;
+	@media (max-width: 990px) {
+		main {
+			height: 30vh;
 		}
-	} */
+	}
 </style>
