065dcbafae3ff7f95494d6f7b418bfc0ebf3d29c
gabrielatwell1987 <gabrielatwell@proton.me>
2024-06-28 16:13:49 -0700

    edited font size on hero logo

--- src/lib/components/HeroLogo.svelte
+++ src/lib/components/HeroLogo.svelte
@@ -42,7 +42,7 @@
 	}
 
 	h1 {
-		font-size: clamp(3rem, 10vw, 12rem);
+		font-size: clamp(2.25rem, 10vw, 12rem);
 		letter-spacing: 5px;
 		font-family: var(--montserrat);
 		color: var(--off-white);
