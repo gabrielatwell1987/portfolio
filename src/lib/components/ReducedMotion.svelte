784ef1dffe3aa8fe95b35e90d9c8a45a320afbbf
gabrielatwell1987 <gabrielatwell@proton.me>
2024-08-04 15:27:16 -0700

    made a prefers reduced motion component

---
+++ src/lib/components/ReducedMotion.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { getPrefersReducedMotion } from './ReducedMotion';
+
+	const prefersReducedMotion = getPrefersReducedMotion();
+
+	const value = prefersReducedMotion ? 'Yes' : 'No';
+</script>
+
+<h1>Prefers Reduced Motion: {value}</h1>
