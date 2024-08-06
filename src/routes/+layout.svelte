02e44c4f22956b213fc88b3a266babf2690aa6a3
gabrielatwell1987 <gabrielatwell@proton.me>
2024-08-06 15:54:03 -0700

    made my website a PWA

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,11 +9,30 @@
 
 	let isPageLoaded = false;
 
+	async function detectSWUpdate() {
+		const registration = await navigator.serviceWorker.ready;
+
+		registration.addEventListener('updatefound', () => {
+			const newSW = registration.installing;
+
+			newSW.addEventListener('statechange', () => {
+				if (newSW.state === 'installed') {
+					if (confirm('New version available! Reload to update?')) {
+						newSW.postMessage({ type: 'SKIP_WAITING' });
+						location.reload();
+					}
+				}
+			});
+		});
+	}
+
 	onMount(async () => {
 		const eruda = (await import('eruda')).default;
 		eruda.init();
 
 		isPageLoaded = true;
+
+		detectSWUpdate();
 	});
 </script>
 
