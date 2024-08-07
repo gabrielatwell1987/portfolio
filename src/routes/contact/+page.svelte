88346017616db37a7f8575ae761fa8d65430265d
gabrielatwell1987 <gabrielatwell@proton.me>
2024-08-07 12:17:50 -0700

    i finally centered the SendButton on the ContactForm component

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -18,66 +18,14 @@
 </main>
 
 <style>
-	.contactForm {
-		margin: 0 auto;
-	}
-
 	@media screen and (min-width: 200px) {
-		.contactForm {
-			display: block;
-			margin: 1rem 0 0 55%;
-		}
-
 		.legend {
-			margin: 10% 0 0 0;
+			margin: 10% 0 5% 0;
 			font-family: var(--anta);
 			font-size: clamp(1.75rem, 4vw, 5rem);
 			letter-spacing: 3%;
 			padding: 0;
 			color: var(--purple);
-		}
-	}
-
-	@media screen and (min-width: 500px) {
-		.contactForm {
-			margin: 1rem 0 0 39%;
-		}
-	}
-
-	@media screen and (min-width: 700px) {
-		.contactForm {
-			margin: 1rem 0 0 40%;
-		}
-	}
-
-	@media screen and (min-width: 850px) {
-		.contactForm {
-			margin: 1rem 0 0 35%;
-		}
-	}
-
-	@media screen and (min-width: 990px) {
-		.contactForm {
-			margin: 1rem 0 0 33%;
-		}
-	}
-
-	@media screen and (min-width: 1024px) {
-		.contactForm {
-			margin: 1rem 0 0 32%;
-		}
-	}
-
-	@media screen and (min-width: 1400px) {
-		.contactForm {
-			margin: 1rem 0 0 20%;
-			scale: 0.8;
-		}
-	}
-
-	@media screen and (min-width: 1600px) {
-		.contactForm {
-			margin: 1rem 0 0 14%;
 		}
 	}
 </style>
