129235d580a6d7938f25fdcf5bf06119b9545dd2
gabrielatwell1987 <gabrielatwell@proton.me>
2024-08-08 16:03:29 -0700

    fixed the responsiveness on hero page

--- src/routes/about/hero/+page.svelte
+++ src/routes/about/hero/+page.svelte
@@ -31,258 +31,6 @@
 </section>
 
 <style>
-	/* @media screen and (min-width: 200px) {
-		section {
-			text-align: center;
-			font-size: 1.75rem;
-			height: 100vh;
-			position: relative;
-			margin-bottom: 2%;
-			margin-top: 50%;
-		}
-
-		h3 {
-			margin: 2rem auto;
-			margin-top: 10%;
-			color: var(--sky);
-			font-size: 1.5rem;
-			font-weight: 600;
-			font-family: var(--anta);
-			letter-spacing: 2px;
-		}
-
-		h4 {
-			color: var(--white);
-			font-weight: 400;
-			letter-spacing: 0.5px;
-			font-family: var(--lexend);
-			font-size: 1rem;
-		}
-
-		button {
-			display: block;
-			background-color: transparent;
-			color: var(--white);
-			font-family: var(--lexend);
-			width: 50%;
-			height: 10%;
-			outline: none;
-			border: 1px solid var(--white);
-			margin: 1rem auto;
-			border-radius: 10px;
-			font-size: 1.2rem;
-			border-radius: 5rem;
-			filter: drop-shadow(0 0 0.75rem var(--smoke));
-			letter-spacing: 1px;
-			transition: 750ms;
-			outline: 3px solid currentColor;
-			outline-offset: -7px;
-			margin-top: 2rem;
-			margin-bottom: 0.25rem;
-			padding: 1rem 1.75rem;
-			user-select: none;
-		}
-
-		button:hover,
-		button:focus {
-			border: 1px solid var(--black);
-			background-color: var(--white);
-			color: var(--black);
-			outline-offset: 0px;
-			animation: wiggle 0.5s ease-in-out infinite;
-		}
-
-		section {
-			position: absolute;
-			top: 50%;
-			left: 50%;
-			transform: translate(-50%, -50%);
-			width: 100%;
-		}
-
-		.btn {
-			text-decoration: none;
-			margin: 0 auto;
-		}
-
-		.summary {
-			margin: 0 auto;
-			font-size: 1rem;
-			font-weight: 500;
-			width: 100%;
-			padding: 0 1.25rem;
-		}
-
-		.italic {
-			font-style: italic;
-			font-family: var(--montserrat);
-			font-weight: 200;
-			font-size: 0.8rem;
-			color: var(--white);
-		}
-
-		.left {
-			text-align: left;
-		}
-
-		@keyframes wiggle {
-			0% {
-				rotate: 0deg;
-				translate: 0 0;
-			}
-			25% {
-				rotate: -2deg;
-				translate: -5px 0;
-			}
-			80% {
-				rotate: 2deg;
-				translate: 5px 0;
-			}
-		}
-	}
-
-	@media screen and (min-width: 740px) {
-		section {
-			text-align: center;
-			font-size: 2rem;
-		}
-
-		button {
-			font-size: 1.25rem;
-			width: 25%;
-			font-size: 1rem;
-			letter-spacing: 2px;
-		}
-
-		button:hover {
-			animation: wiggle 0.5s ease-in-out infinite;
-		}
-
-		h3 {
-			font-size: 2.5rem;
-			letter-spacing: 7px;
-		}
-
-		h4 {
-			color: var(--white);
-			font-size: 1.5rem;
-		}
-
-		.summary {
-			width: 75%;
-		}
-
-		@keyframes wiggle {
-			0% {
-				rotate: 0deg;
-				translate: 0 0;
-			}
-			25% {
-				rotate: -2deg;
-				translate: -5px 0;
-			}
-			80% {
-				rotate: 2deg;
-				translate: 5px 0;
-			}
-		}
-	}
-
-	@media screen and (min-width: 990px) {
-		section {
-			text-align: center;
-			font-size: 2.5rem;
-			margin: 5rem 0;
-		}
-
-		button {
-			width: 35%;
-			font-size: 1.5rem;
-			margin: 3rem auto;
-			padding: 1.5rem 2.5rem;
-		}
-
-		.italic {
-			margin-top: -2rem;
-		}
-
-		.summary {
-			width: 50%;
-			font-size: 1.25rem;
-			letter-spacing: 3px;
-		}
-
-		section {
-			top: 30%;
-			left: 50%;
-			transform: translate(-50%, -30%);
-		}
-
-		h3 {
-			font-size: 2.2rem;
-			letter-spacing: 10px;
-		}
-	}
-
-	@media (min-width: 1024px) {
-		h3 {
-			font-size: 2.25rem;
-			width: 90%;
-			margin-inline: auto;
-		}
-
-		.summary {
-			font-size: 1.25rem;
-			width: 80%;
-			margin-inline: auto;
-		}
-
-		section {
-			top: 35%;
-		}
-
-		button {
-			padding: 1.75rem;
-			text-align: center;
-			display: flex;
-			justify-content: center;
-			align-items: center;
-		}
-	}
-
-	@media screen and (min-width: 1100px) {
-		button {
-			width: 25%;
-		}
-
-		section {
-			top: 30%;
-			scale: 0.85;
-		}
-	}
-
-	@media screen and (min-width: 1400px) {
-		section {
-			top: 20%;
-			left: 42%;
-		}
-	}
-
-	@media screen and (min-width: 1800px) {
-		button {
-			width: 15%;
-			font-size: 2.5rem;
-		}
-
-		h3 {
-			font-size: 5rem;
-		}
-
-		.summary {
-			font-size: 1.75rem;
-		}
-	} */
-
 	@media screen and (min-width: 200px) {
 		section {
 			text-align: center;
@@ -343,18 +91,9 @@
 			animation: wiggle 0.5s ease-in-out infinite;
 		}
 
-		/* img {
-			border-radius: 15px;
-			opacity: 0.25;
-			transition: opacity 0.75s ease-in-out;
-			width: 100%;
-			height: 100%;
-			object-fit: cover;
-		} */
-
 		section {
 			position: absolute;
-			top: 50%;
+			top: 90%;
 			left: 50%;
 			transform: translate(-50%, -50%);
 			width: 100%;
@@ -404,6 +143,7 @@
 		section {
 			text-align: center;
 			font-size: 2rem;
+			top: 85%;
 		}
 
 		button {
@@ -470,7 +210,7 @@
 		}
 
 		section {
-			top: 30%;
+			top: 45%;
 			left: 50%;
 			transform: translate(-50%, -30%);
 		}
@@ -494,7 +234,7 @@
 		}
 
 		section {
-			top: 20%;
+			top: 40%;
 		}
 	}
 
