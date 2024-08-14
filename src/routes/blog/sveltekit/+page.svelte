25c0957f9be9af2609a7a7cfd18b69ebe8c3b4ef
gabrielatwell1987 <gabrielatwell@proton.me>
2024-08-14 11:47:08 -0700

    edited the greensock and sveltekit details animations on the blog pages and aligned the .open-button on ContactForm component

--- src/routes/blog/sveltekit/+page.svelte
+++ src/routes/blog/sveltekit/+page.svelte
@@ -71,14 +71,11 @@
 					of the server-side rendering (SSR) and all of the SEO features, as well as making the
 					website really fast. The fact that this, to me, is less than an actual framework as it
 					relies heavily on vanilla HTML, CSS, and JavaScript is what I'm looking for.
-				</p>
-
-				<p class="detail">
-					In comparing all of the frameworks for the best one to use, my list is 1.) Sveltekit 2.)
-					Astro 3.) React. I like Astro because it is really easy to use, and you can use any other
-					framework's components you want. I can use svelte with astro, react with astro, etc. I put
-					react as #3 because it is really verbose.. but it does the same thing that sveltekit and
-					astro do.
+					<br /><br />
+					In comparing all of the frameworks for the best one to use, my list is 1.) Sveltekit 2.) Astro
+					3.) React. I like Astro because it is really easy to use, and you can use any other framework's
+					components you want. I can use svelte with astro, react with astro, etc. I put react as #3
+					because it is really verbose.. but it does the same thing that sveltekit and astro do.
 				</p>
 			</details>
 
@@ -89,12 +86,10 @@
 					I am very knowledgable to the fundamentals of web development. CSS is a real hobby that I
 					have because I'm a very visual person. Sveltekit has made it super simple to put my ideas
 					on the web.
-				</p>
-
-				<p class="detail">
-					The framework is based off of html. Javascript is used in a script tag, css is used in a
-					style tag, just like you do it inline in html. Everything is scoped, so it makes it really
-					easy to build things.
+					<br /><br />
+					The framework is based off of html. Javascript is used in a script tag, css is used in a style
+					tag, just like you do it inline in html. Everything is scoped, so it makes it really easy to
+					build things.
 				</p>
 			</details>
 
@@ -107,12 +102,10 @@
 					React. In the end, sveltekit is a new approach to building rich user interfaces.
 					Originally, svelte was created as a tool to build components for data visualization. It
 					makes creating data visualizations super easy!
-				</p>
-
-				<p class="detail">
-					Svelte is usually used for SPA's (single page applications), but with sveltekit, you can
-					use it's routing features to create a MPA's (multi-page applications). This is what I did
-					with this website.
+					<br /><br />
+					Svelte is usually used for SPA's (single page applications), but with sveltekit, you can use
+					it's routing features to create a MPA's (multi-page applications). This is what I did with
+					this website.
 				</p>
 			</details>
 
@@ -132,16 +125,12 @@
 					to start a development server and start the project. Now when you make changes to your code
 					and press save, it will update on your development server without having to hit refresh! Also,
 					this starts the project on <code class="code">http://localhost:5173</code>.
-				</p>
-
-				<p class="detail">
+					<br /><br />
 					Once you have the development server running, you can start building your project. You can
 					use the <code class="code">src/routes</code> folder to create new pages. You can use the
-					<code class="code">src/lib</code> folder to store and create new components or data for your
-					project.
-				</p>
-
-				<p class="detail">
+					<code class="code">src/lib</code> folder to store and create new components or data for
+					your project.
+					<br /><br />
 					You can use the <code class="code">+layout.svelte</code> file to create a layout for your
 					project. You can use the <code class="code">+error.svelte</code>
 					file to create an error page. And <code class="code">+page.svelte</code> creates a new page.
