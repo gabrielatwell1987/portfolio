b2b70b3fe49fa00726c446fd035f5d0fff97e867
gabrielatwell1987 <gabrielatwell@proton.me>
2024-08-16 12:07:41 -0700

    created a new Pulsing component and added it as a loading animation on the Loading component

---
+++ src/lib/components/Pulsing.svelte
@@ -0,0 +1,34 @@
+<script>
+</script>
+
+<main class="loader" />
+
+<style>
+	.loader {
+		width: 5rem;
+		aspect-ratio: 1;
+		border-radius: 50%;
+		background: #fff;
+		box-shadow: 0 0 0 0 #fff4;
+		animation: l2 1.5s infinite linear;
+		position: relative;
+	}
+	.loader:before,
+	.loader:after {
+		content: '';
+		position: absolute;
+		inset: 0;
+		border-radius: inherit;
+		box-shadow: 0 0 0 0 #fff4;
+		animation: inherit;
+		animation-delay: -0.5s;
+	}
+	.loader:after {
+		animation-delay: -1s;
+	}
+	@keyframes l2 {
+		100% {
+			box-shadow: 0 0 0 40px #fff6;
+		}
+	}
+</style>
