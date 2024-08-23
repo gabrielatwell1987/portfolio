1804b0e6ec4c9d974619e47f803bb4842da5428f
gabrielatwell1987 <gabrielatwell@proton.me>
2024-08-23 11:39:18 -0700

    in the posts folder, i made a new Heading component that I imported on variables.md.. added the Popover component to the sveltekit page

---
+++ src/routes/blog/posts/Heading.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let title;
+</script>
+
+<span>{title}</span>
+
+<style>
+	span {
+		font-weight: bold;
+		font-size: clamp(1.5rem, 2vw, 2.25rem);
+		color: var(--smoke);
+	}
+</style>
