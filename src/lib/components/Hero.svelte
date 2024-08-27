fd4cdc3e19256bafe0145cef4cc152e254921cf6
gabrielatwell1987 <gabrielatwell@proton.me>
2024-08-27 12:55:04 -0700

    took the aria-hidden off of Hero.svelte

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -19,7 +19,7 @@
 	});
 </script>
 
-<main aria-hidden="true">
+<main>
 	<!-- hero section -->
 	<img
 		src="https://images.pexels.com/photos/196644/pexels-photo-196644.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
