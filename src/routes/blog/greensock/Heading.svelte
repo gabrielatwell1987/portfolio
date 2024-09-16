eb131e242290d24300f760c26933e0fa52f2e871
gabrielatwell1987 <gabrielatwell@proton.me>
2024-09-16 11:06:03 -0700

    deleted and took off variables.md and added it to a new details section called Variables on the greensock page.. also edited Heading.svelte and moved it to the greensock folder.. I also edited the thickness of LoadingDots.svelte

---
+++ src/routes/blog/greensock/Heading.svelte
@@ -0,0 +1,14 @@
+<script>
+	export let title;
+</script>
+
+<span>{title}:</span>
+
+<style>
+	span {
+		font-weight: bold;
+		font-size: clamp(1.5rem, 2vw, 2rem);
+		color: var(--white);
+		margin-right: 1rem;
+	}
+</style>
