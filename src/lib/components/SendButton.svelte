1f7db6bca1946edfc632b55c94aa16b337934a81
gabrielatwell1987 <gabrielatwell@proton.me>
2024-09-18 15:34:33 -0700

    changed the font to orbitron on Button, Grid, SendButton components, and added a new logo and updated HeroLogo.svelte

--- src/lib/components/SendButton.svelte
+++ src/lib/components/SendButton.svelte
@@ -7,13 +7,13 @@
 	@media (min-width: 300px) {
 		.send {
 			background-color: transparent;
-			font-family: var(--lexend);
+			font-family: var(--orbitron);
+			font-size: clamp(1.2rem, 2vw, 2rem);
+			font-weight: 600;
 			color: var(--yellow);
 			border: 1px solid var(--yellow);
 			outline: 3px solid var(--yellow);
 			filter: drop-shadow(0 0 0.75rem var(--smoke));
-			font-size: clamp(1.2rem, 4vw, 2rem);
-			font-weight: 600;
 			margin-inline: auto;
 			cursor: pointer;
 			letter-spacing: 1px;
@@ -79,7 +79,7 @@
 			width: 25%;
 			margin-inline: auto;
 			letter-spacing: 0;
-			font-size: 1.75rem;
+			font-size: 2rem;
 		}
 	}
 
