3956a86788e0ac29e5b8291d17516705636aca79
gabrielatwell1987 <gabrielatwell@proton.me>
2024-09-25 13:36:09 -0700

    testing if my video will play

---
+++ src/lib/components/Video.svelte
@@ -0,0 +1,33 @@
+<script>
+	export let src1;
+	export let src2;
+
+	let duration;
+	let videoWidth;
+	let videoHeight;
+</script>
+
+<div class="video">
+	<video bind:duration bind:videoWidth bind:videoHeight autoplay muted playsinline loop>
+		<source src={src1} type="video/webm" />
+		<source src={src2} type="video/mp4" />
+
+		<track kind="captions" src="/videos/captions.vtt" srclang="en" label="English" />
+
+		Your browser does not support the video tag.
+	</video>
+</div>
+
+<style>
+	video {
+		width: 100%;
+		max-width: 700px;
+		border-radius: 12px;
+	}
+
+	.video {
+		margin: 0 auto 6% auto;
+		display: flex;
+		justify-content: center;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,14 +3,11 @@
 	import HeroLogo from '$lib/components/HeroLogo.svelte';
 	import SEO from '$lib/data/SEO.svelte';
 	import AnimatedIntro from '$lib/components/AnimatedIntro.svelte';
+	import Video from '$lib/components/Video.svelte';
 
 	$: console.log(Hero);
 	$: console.log('Hero section');
 </script>
-
-<svelte:head>
-	<script src="https://cdn.jsdelivr.net/npm/@mux/mux-player"></script>
-</svelte:head>
 
 <SEO
 	title="Gabriel Atwell"
@@ -23,23 +20,13 @@
 <AnimatedIntro text="Do you need a " span="website?" />
 
 <div class="video">
-	<!-- <mux-player
-		playback-id="zGVJu02OI79wCfUlrXFdy2JTEyPD69T8k8snu2i11O500"
-		metadata-video-title="Frontend Developer"
-		metadata-viewer-user-id="atwellUI"
-		accent-color="var(--purple)"
-	/> -->
-	<video src="https://stream.mux.com/zGVJu02OI79wCfUlrXFdy2JTEyPD69T8k8snu2i11O500.m3u8" controls>
-		<track kind="captions" src="path/to/captions.vtt" srclang="en" label="English" />
-	</video>
+	<Video src1="/videos/newFED.webm" src2="/videos/newFED.mp4" />
 </div>
 
 <Hero />
 
 <style>
 	.video {
-		margin: 0 auto;
-		width: 100%;
-		max-width: 800px;
+		border-radius: 12px;
 	}
 </style>
