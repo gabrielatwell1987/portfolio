9723c53f2589bd6d027da8d304aeb7a01c28f856
gabrielatwell1987 <gabrielatwell@proton.me>
2024-09-25 12:35:23 -0700

    testing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,10 @@
 	$: console.log('Hero section');
 </script>
 
+<svelte:head>
+	<script src="https://cdn.jsdelivr.net/npm/@mux/mux-player"></script>
+</svelte:head>
+
 <SEO
 	title="Gabriel Atwell"
 	description="Gabriel Atwell's Portfolio"
@@ -18,4 +22,24 @@
 
 <AnimatedIntro text="Do you need a " span="website?" />
 
+<div class="video">
+	<!-- <mux-player
+		playback-id="zGVJu02OI79wCfUlrXFdy2JTEyPD69T8k8snu2i11O500"
+		metadata-video-title="Frontend Developer"
+		metadata-viewer-user-id="atwellUI"
+		accent-color="var(--purple)"
+	/> -->
+	<video src="https://stream.mux.com/zGVJu02OI79wCfUlrXFdy2JTEyPD69T8k8snu2i11O500.m3u8" controls>
+		<track kind="captions" src="path/to/captions.vtt" srclang="en" label="English" />
+	</video>
+</div>
+
 <Hero />
+
+<style>
+	.video {
+		margin: 0 auto;
+		width: 100%;
+		max-width: 800px;
+	}
+</style>
