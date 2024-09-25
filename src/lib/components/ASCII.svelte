774c5c94fb4e095eb8b413e0bf5d0a0cadc1f997
gabrielatwell1987 <gabrielatwell@proton.me>
2024-09-25 13:58:30 -0700

    i did some touch ups on about page, ImageFrame.svelte, ASCII.svelte

--- src/lib/components/ASCII.svelte
+++ src/lib/components/ASCII.svelte
@@ -5,3 +5,12 @@
 <main>
 	<Image src="/logos/ascii.jpg" alt="ASCII Art" />
 </main>
+
+<style>
+	main {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		max-width: min-content;
+	}
+</style>
