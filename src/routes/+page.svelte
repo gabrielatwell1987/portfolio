1445143d180ee5bbbbc3f2c23b058c44c3ea4828
gabrielatwell1987 <gabrielatwell@proton.me>
2024-10-09 11:01:03 -0700

    testing taking off the video of main page to see if performance is better

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 	import HeroLogo from '$lib/components/HeroLogo.svelte';
 	import SEO from '$lib/data/SEO.svelte';
 	import AnimatedIntro from '$lib/components/AnimatedIntro.svelte';
-	import Video from '$lib/components/Video.svelte';
+	// import Video from '$lib/components/Video.svelte';
 
 	$: console.log('Home page');
 </script>
@@ -18,14 +18,14 @@
 
 <AnimatedIntro text="Do you need a " span="website?" />
 
-<div class="video">
+<!-- <div class="video">
 	<Video src1="/videos/seamless.webm" src2="/videos/seamless.mp4" />
-</div>
+</div> -->
 
 <Hero />
 
 <style>
-	.video {
+	/* .video {
 		border-radius: 12px;
-	}
+	} */
 </style>
