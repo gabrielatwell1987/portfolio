7547f466fcefcc8e4bce4300171a11e1ac019b23
gabrielatwell1987 <gabrielatwell@proton.me>
2024-10-24 12:03:02 -0700

    switched from using google fonts cdn to installing the actual fonts for performance

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -55,13 +55,6 @@
 	/>
 
 	<link rel="canonical" href={canonicalUrl} />
-
-	<link rel="preconnect" href="https://fonts.googleapis.com" />
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-	<link
-		href="https://fonts.googleapis.com/css2?family=Anta&family=Lexend:wght@100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Orbitron:wght@400..900&family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap"
-		rel="stylesheet"
-	/>
 </svelte:head>
 
 <Analytics />
