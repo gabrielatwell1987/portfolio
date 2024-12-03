4fe781a729df2a6d84e02e9aa32406a29563001c
gabrielatwell1987 <gabrielatwell@proton.me>
2024-12-03 13:42:33 -0800

    testing installation button

--- src/service-worker.js
+++ src/service-worker.js
@@ -74,3 +74,10 @@ self.addEventListener('message', (event) => {
 		self.skipWaiting();
 	}
 });
+
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker
+		.register('/service-worker.js')
+		.then(() => console.log('Service Worker registered'))
+		.catch((err) => console.error('Service Worker registration failed:', err));
+}
