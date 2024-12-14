e8688a312b69c2a809f61e435c66a52b08307e90
gabrielatwell1987 <gabrielatwell@proton.me>
2024-12-14 12:24:44 -0800

    testing this out again

--- src/lib/components/Video.svelte
+++ src/lib/components/Video.svelte
@@ -1,4 +1,4 @@
-<script>
+<!-- <script>
 	/** @type {{src: any, src2: any}} */
 	let { src, src2 } = $props();
 
@@ -13,7 +13,6 @@
 
 		<source {src2} type="video/mp4" />
 
-		<!-- <source src={src.replace('.webm', '.mp4')} type="video/webm" /> -->
 
 		<p>The video has failed to load...</p>
 	</video>
@@ -31,8 +30,57 @@
 			min-width: 320px;
 			max-width: 1200px;
 			border-radius: 12px;
-			/* mask-image: none; */
 
+			mask-image: linear-gradient(transparent, black 20%),
+				linear-gradient(to top, transparent, black 20%),
+				linear-gradient(to right, transparent, black 20%),
+				linear-gradient(to left, transparent, black 20%);
+			mask-composite: intersect;
+		}
+	}
+
+	@media (max-width: 700px) {
+		.video {
+			margin: 0 auto 18% auto;
+
+			video {
+				max-width: 85%;
+			}
+		}
+	}
+</style> -->
+
+<script>
+	/** @type {{src: any}} */
+	let { src } = $props();
+
+	let duration = $state();
+	let videoWidth = $state();
+	let videoHeight = $state();
+</script>
+
+<div class="video">
+	<video bind:duration bind:videoWidth bind:videoHeight autoplay muted playsinline loop>
+		<source {src} type="video/mp4" />
+
+		<source src={src.replace('.mp4', '.webm')} type="video/webm" />
+
+		<p>Your browser does not support this video..</p>
+	</video>
+</div>
+
+<style>
+	.video {
+		margin-inline: auto;
+		margin-block: 3rem;
+		display: flex;
+		justify-content: center;
+
+		video {
+			width: 90%;
+			min-width: 320px;
+			max-width: 1200px;
+			border-radius: 12px;
 			mask-image: linear-gradient(transparent, black 20%),
 				linear-gradient(to top, transparent, black 20%),
 				linear-gradient(to right, transparent, black 20%),
