1fc394b68186dee3c37134c0975a6fc8731717ed
gabrielatwell1987 <gabrielatwell@proton.me>
2024-12-17 14:26:11 -0800

    added gen3.webp and changed the background image on Hero

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -3,10 +3,11 @@
 </script>
 
 <main>
-	<img
+	<img src="/logos/gen3.webp" alt="generative art" />
+	<!-- <img
 		src="https://images.pexels.com/photos/574073/pexels-photo-574073.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
 		alt="Wireframing and design of a website"
-	/>
+	/> -->
 
 	<section aria-label="hero">
 		<h1 class="hero-title">Frontend Crafted Web Experiences</h1>
