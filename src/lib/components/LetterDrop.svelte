d9fa5f92dc996b03a0d53d443d47ec91a30b0f14
gabrielatwell1987 <gabrielatwell@proton.me>
2024-12-18 12:23:52 -0800

    updated LetterDrop with gsap and changed the size of Title on mobile

--- src/lib/components/LetterDrop.svelte
+++ src/lib/components/LetterDrop.svelte
@@ -4,6 +4,7 @@
 
 	$effect(() => {
 		const letters = gsap.utils.toArray('.animated-letter');
+		const drop = gsap.utils.toArray('.drop');
 
 		gsap.set(letters, {
 			scale: 0,
@@ -22,26 +23,38 @@
 				from: 'edges'
 			}
 		});
+
+		gsap.from(drop, {
+			y: -30,
+			duration: 2,
+			ease: 'sine.inOut',
+			stagger: {
+				each: 0.5,
+				from: 'start'
+			}
+		});
 	});
 </script>
 
 <h1 class="animated-text" transition:fade={{ duration: 500 }}>
 	<div class="tilt">
-		<span class="animated-letter"> a </span>
-		<span class="animated-letter"> b </span>
-		<span class="animated-letter"> o </span>
-		<span class="animated-letter"> u </span>
-		<span class="animated-letter"> t </span>
+		<span class="animated-letter drop">a</span>
+		<span class="animated-letter drop">b</span>
+		<span class="animated-letter drop">o</span>
+		<span class="animated-letter drop">u</span>
+		<span class="animated-letter drop">t</span>
 	</div>
+
 	<span class="animated-letter space"></span>
-	<span class="animated-letter"> m </span>
-	<span class="animated-letter"> e </span>
+
+	<span class="animated-letter">m</span>
+	<span class="animated-letter">e</span>
 </h1>
 
 <style>
 	.animated-text {
 		font-family: var(--anta);
-		font-size: clamp(2.5rem, 9vw, 20rem);
+		font-size: clamp(2rem, 9vw, 20rem);
 		color: var(--sky);
 		mix-blend-mode: lighten;
 		text-align: center;
@@ -56,7 +69,7 @@
 
 		.tilt {
 			transform: rotate(-10deg);
-			font-size: clamp(2.3rem, 9vw, 20rem);
+			font-size: clamp(2.3rem, 15vw, 20rem);
 			display: inline-block;
 			text-shadow: 5px 5px 5px var(--dark-gray);
 			filter: drop-shadow(0px 0px 0.75rem var(--white));
@@ -74,18 +87,6 @@
 			.tilt {
 				text-shadow: 2px 2px 0px #666;
 			}
-		}
-	}
-
-	@keyframes drop {
-		0% {
-			transform: translateY(-100%);
-			opacity: 0;
-		}
-
-		100% {
-			transform: translateY(0);
-			opacity: 1;
 		}
 	}
 </style>
