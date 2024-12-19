1cfbd1f6cdef43368d072e2deaf3586f0b598fa5
gabrielatwell1987 <gabrielatwell@proton.me>
2024-12-18 16:59:45 -0800

    refactored the css of Photos and Modal

--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -35,7 +35,7 @@
 	<i class="fa-regular fa-folder-open"></i>
 </button>
 
-<dialog class="modal" id="modal" aria-label="modal">
+<dialog id="modal" aria-label="modal">
 	<article>
 		<header>
 			<button rel="prev" class="close-button" aria-label="close button">
@@ -45,9 +45,11 @@
 			<h4>Please contact me for any frontend work!</h4>
 		</header>
 
-		<Image src="/logos/newspaper.webp" alt="frontend newspaper clipping" width="500" />
+		<div class="image">
+			<Image src="/logos/newspaper.webp" alt="frontend newspaper clipping" width="500" />
+		</div>
 
-		<p class="modal__p">
+		<p>
 			If you need help with a website, or if you have any questions, please feel free to contact me.
 			I am always happy to help!
 		</p>
@@ -60,8 +62,7 @@
 			margin-bottom: 25%;
 		}
 
-		.open-button,
-		.close-button {
+		.open-button {
 			display: block;
 			font-size: clamp(2.5rem, 4vw, 4rem);
 			font-weight: 800;
@@ -78,73 +79,98 @@
 			align-items: center;
 			margin-inline: auto;
 			cursor: pointer;
+
+			&:hover {
+				animation: wiggle 01s ease-in-out infinite;
+			}
+
+			&:focus {
+				box-shadow: 0 0 0px var(--off-white);
+			}
+
+			& i {
+				cursor: pointer;
+			}
 		}
 
-		.open-button:hover,
-		.close-button:hover {
-			animation: wiggle 01s ease-in-out infinite;
-		}
-
-		.open-button:focus,
-		.close-button:focus {
-			box-shadow: 0 0 0px var(--off-white);
-		}
-
-		.open-button i,
-		.close-button i {
-			cursor: pointer;
-		}
-
-		dialog::backdrop {
-			background: transparent;
+		.close-button {
+			display: block;
+			font-size: clamp(2.5rem, 4vw, 4rem);
+			font-weight: 800;
+			background-color: transparent;
 			box-shadow: none;
+			color: var(--pale);
+			width: 5%;
+			height: 5%;
+			outline: none;
+			border: none;
+			border-radius: 15px;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			margin-inline: auto;
+			margin-top: -1rem;
+			cursor: pointer;
+
+			&:hover {
+				animation: wiggle 01s ease-in-out infinite;
+			}
+
+			&:focus {
+				box-shadow: 0 0 0px var(--off-white);
+			}
+
+			& i {
+				cursor: pointer;
+			}
 		}
 
-		article {
-			width: 100%;
-			border-radius: 1rem;
-			background: var(--blackest);
-			box-shadow: 0 0 5px 3px var(--dark-blue);
-		}
-
-		header {
-			background: transparent;
-		}
-
-		.modal {
+		dialog {
 			background: none;
-		}
 
-		.modal h4 {
-			font-family: var(--anta);
-			font-size: clamp(1.75rem, 1.75vw, 3rem);
-			font-weight: 600;
-			line-height: 1;
-			color: var(--sky);
-			margin: 2rem 0 -0.75rem 0;
-		}
+			&::backdrop {
+				background: transparent;
+				box-shadow: none;
+			}
 
-		.modal__p {
-			font-family: var(--bronova);
-			font-size: clamp(1.15rem, 1.25vw, 2rem);
-			font-weight: 100;
-			color: var(--white);
-			background: transparent;
-			padding: 0.2rem;
+			article {
+				width: 100%;
+				border-radius: 1rem;
+				background: var(--blackest);
+				box-shadow: 0 0 5px 3px var(--dark-blue);
+
+				header {
+					background: transparent;
+					border-bottom: none;
+
+					h4 {
+						font-family: var(--anta);
+						font-size: clamp(1.5rem, 1.75vw, 3rem);
+						font-weight: 600;
+						line-height: 1;
+						color: var(--purple);
+						margin: -0.5rem 0 -2rem 0;
+					}
+				}
+
+				p {
+					font-family: var(--bronova);
+					font-size: clamp(1.15rem, 1.25vw, 2rem);
+					font-weight: 100;
+					color: var(--white);
+					background: transparent;
+					padding: 0.2rem;
+				}
+			}
 		}
 	}
 
 	@media screen and (min-width: 720px) {
-		.modal {
+		dialog {
 			padding: 2rem;
 			width: 93%;
 			height: 83%;
 			margin: 0 auto;
-		}
-
-		.modal__p {
-			width: 60%;
-			margin: 1.5rem auto;
 		}
 
 		.open-button {
@@ -153,15 +179,18 @@
 	}
 
 	@media screen and (min-width: 1024px) {
-		.modal h4 {
+		h4 {
 			line-height: 0.95;
 		}
 
-		.modal__p {
+		p {
 			line-height: 1.75;
-			width: 75%;
-			margin: 0 auto;
+			margin-inline: auto;
 			padding: 1rem;
+		}
+
+		.image {
+			margin-block: 1rem;
 		}
 
 		.open-button {
@@ -170,7 +199,7 @@
 	}
 
 	@media screen and (min-width: 1100px) {
-		.modal h4 {
+		h4 {
 			padding: 0.5rem;
 		}
 	}
