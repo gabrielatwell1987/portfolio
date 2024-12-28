eeaa730977583f56b2f6d5f64fcc820d71e7b8ae
gabrielatwell1987 <gabrielatwell@proton.me>
2024-12-28 15:34:17 -0800

    created Divider and added it to about page

---
+++ src/lib/components/Divider.svelte
@@ -0,0 +1,17 @@
+<script>
+	let { width, thickness } = $props();
+</script>
+
+<hr class="divider" style="--width: {width}%; --thickness: {thickness}px;" />
+
+<style>
+	.divider {
+		width: var(--width);
+		background-color: var(--white);
+		height: var(--thickness);
+		margin-inline: auto;
+		margin-bottom: 10rem;
+		border: none;
+		opacity: 0.5;
+	}
+</style>
