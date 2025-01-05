a129c84a82ecca2fe244d80c679210c8f5aa72ec
gabrielatwell1987 <gabrielatwell@proton.me>
2025-01-05 14:10:44 -0800

    changed the icon alignment on the blog page and made CSS-Purple.webp smaller

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -54,139 +54,146 @@
 <style>
 	main {
 		padding-top: 5%;
-	}
-
-	article {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		flex-direction: column;
-		gap: 20%;
-		background: transparent;
-		box-shadow: none;
-
-		a {
-			display: block;
-			margin: 0 0 2rem 50%;
-		}
-
-		a:focus {
-			box-shadow: 0 0 0px var(--off-white);
-			background: transparent;
-		}
-
-		.gs:not(:hover),
-		.sk:not(:hover) {
-			filter: opacity(0.5);
-			transition: filter 1s ease-in-out;
-		}
-
-		.image:hover {
-			animation: wiggle 1s ease-in-out infinite;
-		}
-
-		.image:not(:hover) {
-			opacity: 0.5;
-			background-color: var(--smoke);
-			color: black;
-			transition: opacity 0.75s ease-in-out;
-		}
-
-		.animation-link {
-			margin-top: 10%;
-		}
-	}
-
-	.image {
-		background-color: transparent;
-		color: var(--smoke);
-		border: 1px solid var(--smoke);
-		width: 40%;
-		margin: 0 auto;
-		border-radius: 8px;
-		cursor: pointer;
-		letter-spacing: 1px;
-	}
-
-	@media (min-width: 300px) {
-		.image {
-			width: 15rem;
-			margin-right: 10%;
-			margin: 0 -95%;
-			scale: 1.1;
-		}
-
-		.gs {
-			margin-bottom: 5rem;
-		}
-
-		.gs,
-		.sk {
-			width: 10rem;
-			margin-inline: auto;
-		}
-
-		.animation-link {
-			display: block;
-		}
-
-		.margin {
-			margin-top: 15%;
-			margin-bottom: 25%;
-		}
-	}
-
-	@media (min-width: 500px) {
-		.image {
-			width: 60%;
-			margin: 0 -30%;
-		}
-	}
-
-	@media (min-width: 720px) {
-		.image {
-			width: 40%;
-			margin: 0 -21%;
-		}
-
-		.gs {
-			margin-bottom: 12rem;
-		}
-
-		.gs,
-		.sk,
-		.image {
-			margin-top: 0;
-		}
-	}
-
-	@media (min-width: 990px) {
-		.gs,
-		.sk {
-			width: 75%;
-			margin-bottom: 35%;
-		}
-
-		.image {
-			margin-top: -20%;
-			scale: 1.3;
-		}
-
-		main {
-			margin-bottom: -12%;
-		}
-
-		.animation-link {
-			margin-bottom: 0;
-		}
 
 		article {
-			margin-top: 2%;
-			margin-right: 5%;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			flex-direction: column;
+			gap: 20%;
+			background: transparent;
+			box-shadow: none;
+
+			a {
+				display: block;
+				margin: 0 0 2rem 50%;
+
+				&:focus {
+					box-shadow: 0 0 0px var(--off-white);
+					background: transparent;
+				}
+			}
+
+			.gs,
+			.sk {
+				transition: scale 0.25s ease-in-out;
+			}
+
+			.gs:not(:hover),
+			.sk:not(:hover) {
+				filter: opacity(0.5);
+				scale: 0.95;
+				transition:
+					filter 1s ease-in-out,
+					scale 0.25s ease-in-out;
+			}
+
+			.image:hover {
+				animation: wiggle 1s ease-in-out infinite;
+			}
+
+			.image:not(:hover) {
+				opacity: 0.5;
+				background-color: var(--smoke);
+				color: black;
+				transition: opacity 0.75s ease-in-out;
+			}
+
+			.animation-link {
+				margin-top: 10%;
+			}
 		}
 
-		.margin {
-			margin-bottom: 13%;
+		.image {
+			background-color: transparent;
+			color: var(--smoke);
+			border: 1px solid var(--smoke);
+			width: 40%;
+			margin: 0 auto;
+			border-radius: 8px;
+			cursor: pointer;
+			letter-spacing: 1px;
+		}
+
+		@media (min-width: 300px) {
+			.image {
+				width: 15rem;
+				margin-right: 10%;
+				margin: 0 -95%;
+				scale: 1.1;
+			}
+
+			.gs {
+				margin-bottom: 5rem;
+			}
+
+			.gs,
+			.sk {
+				width: 10rem;
+				margin-inline: auto;
+			}
+
+			.animation-link {
+				display: block;
+			}
+
+			.margin {
+				margin-top: 15%;
+				margin-bottom: 25%;
+			}
+		}
+
+		@media (min-width: 500px) {
+			.image {
+				width: 60%;
+				margin: 0 -30%;
+			}
+		}
+
+		@media (min-width: 720px) {
+			.image {
+				width: 40%;
+				margin: 0 -21%;
+			}
+
+			.gs {
+				margin-bottom: 12rem;
+			}
+
+			.gs,
+			.sk,
+			.image {
+				margin-top: 0;
+			}
+		}
+
+		@media (min-width: 990px) {
+			.gs,
+			.sk {
+				width: 75%;
+				margin-bottom: 35%;
+			}
+
+			.image {
+				margin-top: -20%;
+				scale: 1.3;
+			}
+
+			& {
+				margin-bottom: -12%;
+			}
+
+			.animation-link {
+				margin-bottom: 0;
+			}
+
+			article {
+				margin-top: 2%;
+			}
+
+			.margin {
+				margin-bottom: 13%;
+			}
 		}
 	}
 
