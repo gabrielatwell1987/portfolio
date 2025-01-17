f435d060d5797541c4560e8dab20972af193415e
gabrielatwell1987 <gabrielatwell@proton.me>
2025-01-17 13:20:19 -0800

    made ASCII centered, created HeroImage but I need 16/9 pics to successfully utilize it

--- src/lib/components/ASCII.svelte
+++ src/lib/components/ASCII.svelte
@@ -1,16 +1,30 @@
 <script>
-	import Image from '$lib/components/Image.svelte';
+	// import Image from '$lib/components/Image.svelte';
+	import gsap from 'gsap';
+
+	$effect(() => {
+		const asciiTl = gsap.timeline();
+
+		asciiTl.from('.ascii', {
+			opacity: 0,
+			scale: 1.2,
+			x: -100,
+			duration: 5,
+			ease: 'slow(0.7, 0.7, false)'
+		});
+	});
 </script>
 
-<main>
-	<Image src="/logos/ascii.jpg" alt="ASCII Art" />
+<main class="ascii">
+	<img src="/logos/ascii.webp" alt="ASCII Art" />
 </main>
 
 <style>
-	main {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		max-width: min-content;
+	.ascii {
+		width: 100%;
+		height: 100%;
+		display: grid;
+		place-items: center;
+		margin: 0;
 	}
 </style>
