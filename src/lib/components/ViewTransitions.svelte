e7ed89d06d6b383623843c37203f228ebc9be54f
gabrielatwell1987 <gabrielatwell@proton.me>
2025-01-22 16:40:24 -0800

    i'm trying to target safari devices and made the opacity higher

--- src/lib/components/ViewTransitions.svelte
+++ src/lib/components/ViewTransitions.svelte
@@ -26,7 +26,43 @@
 		animation: 1.5s cubic-bezier(0, 0, 0.2, 1) 1s both in;
 	}
 
+	@supports (-webkit-touch-callout: none) {
+		:root::view-transition-old(root) {
+			background-color: #000;
+			animation: 1.5s cubic-bezier(0.4, 0, 1, 1) both out-safari;
+		}
+
+		:root::view-transition-new(root) {
+			background-color: #000;
+			animation: 1.5s cubic-bezier(0, 0, 0.2, 1) 1s both in-safari;
+		}
+	}
+
 	@keyframes in {
+		from {
+			scale: 0.97;
+			opacity: 0.1;
+		}
+
+		to {
+			scale: 1;
+			opacity: 1;
+		}
+	}
+
+	@keyframes out {
+		from {
+			scale: 1;
+			opacity: 1;
+		}
+
+		to {
+			scale: 0.97;
+			opacity: 0.1;
+		}
+	}
+
+	@keyframes in-safari {
 		from {
 			scale: 0.97;
 			opacity: 0.75;
@@ -38,7 +74,7 @@
 		}
 	}
 
-	@keyframes out {
+	@keyframes out-safari {
 		from {
 			scale: 1;
 			opacity: 1;
