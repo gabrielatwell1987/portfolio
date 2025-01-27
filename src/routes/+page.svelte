d0dddeece58f6f06404d9ea260190d8851ac95f2
gabrielatwell1987 <gabrielatwell@proton.me>
2025-01-27 14:25:27 -0800

    added more keywords to the main page SEO

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,7 @@
 <SEO
 	title="Gabe Atwell"
 	description="Gabriel Atwell's Portfolio"
-	keywords="gabriel atwell, gabe atwell, frontend developer, frontend dev portfolio, frontend developer portfolio, frontend portfolio projects, frontend portfolio, frontend web developer portfolio, junior web developer portfolio, web developer, web designer, web developer, web development, web design, gabe atwell portfolio, gabe atwell developer, gabe atwell web developer, gabe atwell web designer, atwell design, gabe atwell web design, atwell ui"
+	keywords="gabriel atwell, gabe atwell, frontend developer, frontend dev portfolio, frontend developer portfolio, frontend portfolio projects, frontend portfolio, frontend web developer portfolio, junior web developer portfolio, web developer, web designer, web developer, web development, web design, gabe atwell portfolio, gabe atwell developer, gabe atwell web developer, gabe atwell web designer, atwell design, gabe atwell web design, atwell ui, user interface design, web user interface design, web application user interface design, create user interface"
 />
 
 <Intro />
