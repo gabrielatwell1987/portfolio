d64a8415c15bbcf252e1ee475ff08c09e7d8b257
gabrielatwell1987 <gabrielatwell@proton.me>
2025-01-29 14:51:58 -0800

    took text decoration off of the links of Button, Project, and Avatar

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -18,52 +18,56 @@
 
 <style>
 	@media (width >= 300px) {
-		button {
-			background-color: transparent;
-			font-family: var(--orbitron);
-			font-size: clamp(1.2rem, 1.5vw, 2rem);
-			font-weight: 900;
-			color: var(--pale);
-			border: 1px solid var(--pale);
-			outline: 3px solid var(--pale);
-			filter: drop-shadow(0 0 0.75rem var(--smoke));
-			margin-inline: auto;
-			cursor: pointer;
-			letter-spacing: 1px;
-			width: fit-content;
-			border-radius: 0.75rem;
-			transition: 750ms;
-			outline-offset: -7px;
-			padding: clamp(0.4em, 1.5vw, 0.3em) clamp(1em, 2vw, 0.7em);
-			margin-top: 10%;
-			margin-bottom: 2%;
-			user-select: none;
-			cursor: pointer;
+		a {
+			text-decoration: none;
 
-			&:hover,
-			&:focus {
-				opacity: 0.75;
-				outline-offset: 0px;
-				animation: wiggle 1s ease-in-out infinite;
+			& p {
+				margin: 0;
+				font-size: clamp(1.2rem, 1.5vw, 2rem);
+				font-weight: 900;
+				color: var(--pale);
+
+				&:hover svg {
+					animation: right 1s ease-in-out infinite;
+					opacity: 0.75;
+				}
+
+				& button {
+					background-color: transparent;
+					font-family: var(--orbitron);
+					font-size: clamp(1.2rem, 1.5vw, 2rem);
+					font-weight: 900;
+					color: var(--pale);
+					border: 1px solid var(--pale);
+					outline: 3px solid var(--pale);
+					filter: drop-shadow(0 0 0.75rem var(--smoke));
+					margin-inline: auto;
+					cursor: pointer;
+					letter-spacing: 1px;
+					width: fit-content;
+					border-radius: 0.75rem;
+					transition: 750ms;
+					outline-offset: -7px;
+					padding: clamp(0.4em, 1.5vw, 0.3em) clamp(1em, 2vw, 0.7em);
+					margin-top: 10%;
+					margin-bottom: 2%;
+					user-select: none;
+					cursor: pointer;
+
+					&:hover,
+					&:focus {
+						opacity: 0.75;
+						outline-offset: 0px;
+						animation: wiggle 1s ease-in-out infinite;
+					}
+				}
+
+				@media (width <= 500px) {
+					button svg {
+						display: none;
+					}
+				}
 			}
-		}
-
-		@media (width <= 500px) {
-			button svg {
-				display: none;
-			}
-		}
-	}
-
-	p {
-		margin: 0;
-		font-size: clamp(1.2rem, 1.5vw, 2rem);
-		font-weight: 900;
-		color: var(--pale);
-
-		&:hover svg {
-			animation: right 1s ease-in-out infinite;
-			opacity: 0.75;
 		}
 	}
 
