1c056b7733f91a3a62fb2e2dff8aea2e09b4d2a4
gabrielatwell1987 <gabrielatwell@proton.me>
2025-01-30 14:20:38 -0800

    deleted unused logo and made Parallax with gsap

---
+++ src/lib/components/Parallax.svelte
@@ -0,0 +1,78 @@
+<script>
+	import gsap from 'gsap';
+	import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+
+	let imageElement = $state(null);
+
+	$effect(() => {
+		gsap.registerPlugin(ScrollTrigger);
+
+		const parallaxImage = document.querySelector('.parallax-image');
+
+		gsap.to(parallaxImage, {
+			scrollTrigger: {
+				trigger: '.sticky-wrapper',
+				start: 'top top',
+				end: 'bottom top',
+				scrub: true,
+				pin: true
+			},
+			x: 100,
+			y: '15vh',
+			scale: 3,
+			ease: 'none'
+		});
+	});
+</script>
+
+<div class="spacer"></div>
+
+<div class="sticky-wrapper">
+	<div class="sticky-container">
+		<div class="parallax-image" bind:this={imageElement}></div>
+	</div>
+</div>
+
+<div class="spacer"></div>
+
+<style>
+	.spacer {
+		height: 100vh;
+
+		&:nth-child(2) {
+			height: 200vh;
+		}
+	}
+
+	.sticky-wrapper {
+		height: 200vh;
+		position: relative;
+
+		.sticky-container {
+			position: relative;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			height: 100vh;
+
+			.parallax-image {
+				width: 300px;
+				height: 400px;
+				background-color: var(--white);
+				background-size: cover;
+				background-position: center;
+
+				-webkit-mask-image: url('/icons/skull-icon.svg');
+				-webkit-mask-size: contain;
+				-webkit-mask-repeat: no-repeat;
+				-webkit-mask-position: center;
+				mask-image: url('/icons/skull-icon.svg');
+				mask-size: contain;
+				mask-repeat: no-repeat;
+				mask-position: center;
+
+				transition: transform 0.3s ease-out;
+			}
+		}
+	}
+</style>
