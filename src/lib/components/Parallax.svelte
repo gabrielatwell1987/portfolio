31b3631183071dbc02a98e835681a72a9c4ef7a5
gabrielatwell1987 <gabrielatwell@proton.me>
2025-01-31 14:25:50 -0800

    testing the deploy of Parallax

--- src/lib/components/Parallax.svelte
+++ src/lib/components/Parallax.svelte
@@ -39,10 +39,11 @@
 	.sticky-wrapper {
 		height: 100vh;
 		position: relative;
-		background-image: url('logos/skullLaptop.svg');
+		background-image: url('/logos/skullLaptop.svg');
 		background-size: contain;
 		background-position: center;
 		background-repeat: no-repeat;
+		z-index: 3;
 
 		.sticky-container {
 			position: relative;
