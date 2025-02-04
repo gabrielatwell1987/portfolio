397614f4c55f71455416afc28741ae0acac26521
gabrielatwell1987 <gabrielatwell@proton.me>
2025-02-04 14:45:06 -0800

    reverted Photos back to how it was and changed the widths and heights of images

--- src/lib/components/Photos.svelte
+++ src/lib/components/Photos.svelte
@@ -21,6 +21,31 @@
 			tl.to(pictures, { autoAlpha: 1, duration: 2 });
 		});
 
+		mm.add('(min-width: 768px', () => {
+			gsap.set(pictures, { autoAlpha: 0 });
+			gsap.set(gabeElements, { scale: 1, x: '-30%', autoAlpha: 0 });
+
+			let tl = gsap.timeline({ defaults: { duration: 1.5 } });
+
+			tl.to(gabeElements, {
+				x: 0,
+				stagger: 0.5,
+				scale: 0.8,
+				duration: 4,
+				repeat: 1,
+				yoyo: true,
+				autoAlpha: 1,
+				scrollTrigger: {
+					trigger: 'pictures',
+					start: 'top 0%',
+					end: '+=400%',
+					toggleActions: 'play none none reverse',
+					stagger: 0.75,
+					scrub: 1
+				}
+			}).to(pictures, { autoAlpha: 1, duration: 2 });
+		});
+
 		return () => {
 			gsap.killTweensOf(main);
 			gsap.killTweensOf(pictures);
@@ -32,28 +57,22 @@
 <main>
 	<Title title="images" />
 
+	<h2>scroll down</h2>
+
 	<section>
 		<div class="pictures" aria-label="pictures">
 			<aside class="flex">
 				<img
 					class="gabe"
-					src="photos/gabe.webp"
+					src="/photos/gabe.webp"
 					alt="Gabriel Atwell"
-					width="350"
-					height="450"
 					loading="lazy"
+					style="width: 50em; height: 30em;"
 				/>
 
-				<img
-					class="gabe"
-					src="photos/gabe2.webp"
-					alt="Gabriel's face"
-					width="300"
-					height="350"
-					loading="lazy"
-				/>
+				<img class="gabe" src="/photos/gabe2.webp" alt="Gabriel's face" loading="lazy" />
 
-				<img src="/photos/gabe5.webp" alt="Gabriel" class="gabe" width="400" loading="lazy" />
+				<img src="/photos/gabe5.webp" alt="Gabriel" class="gabe" loading="lazy" />
 			</aside>
 		</div>
 
@@ -61,25 +80,24 @@
 			<aside class="flex">
 				<img
 					class="gabe ar58"
-					src="photos/gabe4.webp"
+					src="/photos/gabe4.webp"
 					alt="Gabriel in a restaurant"
-					width="400"
 					loading="lazy"
+					style="width: 50em; height: 30em;"
 				/>
 
 				<img
 					src="/photos/autumn.webp"
 					alt="Gabe and his daughter"
 					class="gabe ar58"
-					width="400"
 					loading="lazy"
+					style="width: 50em; height: 30em;"
 				/>
 
 				<img
 					class="gabe ar58"
-					src="photos/archie.webp"
+					src="/photos/archie.webp"
 					alt="Gabe's poodle Archie"
-					width="450"
 					loading="lazy"
 				/>
 			</aside>
@@ -109,15 +127,9 @@
 
 		<div class="pictures" aria-label="pictures">
 			<aside class="flex">
-				<img
-					src="/photos/wtf3.webp"
-					alt="when gabe was 5 years old"
-					class="gabe"
-					width="425"
-					loading="lazy"
-				/>
+				<img src="/photos/wtf3.webp" alt="when gabe was 5 years old" class="gabe" loading="lazy" />
 
-				<img class="gabe" src="/logos/monkey.webp" alt="monkey" width="550" />
+				<img class="gabe" src="/logos/monkey.webp" alt="monkey" loading="lazy" />
 			</aside>
 		</div>
 	</section>
@@ -132,6 +144,18 @@
 		main {
 			padding-top: 5%;
 			position: relative;
+
+			h2 {
+				color: var(--text-gray);
+				border-bottom: 1px solid var(--text-gray);
+				font-family: var(--bronova);
+				font-size: clamp(0.9rem, 1.5vw, 2rem);
+				font-weight: 300;
+				letter-spacing: 0.1em;
+				margin: 0;
+				width: fit-content;
+				margin-inline: auto;
+			}
 
 			section {
 				width: var(--100);
@@ -156,28 +180,25 @@
 					margin-bottom: 2%;
 					align-items: center;
 
-					img {
+					/* & img {
 						max-width: var(--100);
-					}
+					} */
 
 					.gabe {
 						margin-inline: auto;
 						margin-bottom: 3rem;
 						max-width: 20em;
 						border-radius: 3%;
-						filter: drop-shadow(0);
-						transition:
-							filter 500ms ease-in-out,
-							opacity 200ms ease-in-out,
-							transform 1s ease-in-out;
-						opacity: 0.75;
-						transform: translate(0, 0);
 
 						&:hover {
 							filter: drop-shadow(0 0 0.25rem var(--text-gray));
 							opacity: 1;
-							transform: scale(1.1);
-							z-index: 10;
+						}
+
+						&:not(:hover) {
+							filter: drop-shadow(0);
+							transition: filter 750ms ease-in-out;
+							opacity: 0.75;
 						}
 
 						&.ar58 {
