b361802da6af2392480622e85737e4fc2121ec2e
gabrielatwell1987 <gabrielatwell@proton.me>
2025-02-11 13:07:19 -0800

    added view transitions throughout the whole site so it isn't as boring as it was

--- src/routes/learn/posts/+page.svelte
+++ src/routes/learn/posts/+page.svelte
@@ -6,10 +6,24 @@
 	import Card from '$lib/components/Card.svelte';
 	import Image from '$lib/components/Image.svelte';
 
+	let mounted = $state(false);
+
 	$effect(() => {
 		console.log(GSAP, CSS);
+
+		if (document.startViewTransition) {
+			document.startViewTransition(() => {
+				mounted = true;
+			});
+		} else {
+			mounted = true;
+		}
 	});
 </script>
+
+<svelte:head>
+	<meta name="view-transition" content="same-origin" />
+</svelte:head>
 
 <SEO
 	title="Web animation techniques"
@@ -17,7 +31,7 @@
 	keywords="animation techniques, web techniques for animation"
 />
 
-<section>
+<section class:mounted>
 	<div class="css">
 		<div class="image">
 			<div class="skillImg">
@@ -25,8 +39,6 @@
 					<Image src="/skills/CSS-Purple.webp" alt="css" />
 				</h1>
 			</div>
-
-			<br />
 
 			<CSS aria-label="css" />
 		</div>
@@ -38,8 +50,6 @@
 				<Image src="/skills/GSAP-Dark.svg" alt="gsap" />
 			</h1>
 		</div>
-
-		<br />
 
 		<GSAP aria-label="greensock" />
 	</div>
@@ -60,34 +70,41 @@
 
 <style>
 	section {
-		.card {
+		&.mounted {
+			opacity: 1;
+			scale: 1;
+		}
+
+		& .card {
 			display: flex;
 			justify-content: center;
 			background: transparent;
 			box-shadow: none;
 		}
 
-		.css {
+		& .css {
 			margin-bottom: 15%;
 
-			.image {
+			& .image {
 				margin-top: 5%;
 
-				.skillImg {
+				& .skillImg {
 					width: 25%;
 					margin-inline: auto;
 					margin-block: 7rem;
+					view-transition-name: css-skill-image;
 				}
 			}
 		}
 
-		.gsap {
+		& .gsap {
 			margin-top: -5%;
 			margin-bottom: 5%;
 
-			.skillImg {
+			& .skillImg {
 				margin-inline: auto;
 				margin-top: 6rem;
+				view-transition-name: gsap-skill-image;
 			}
 		}
 
@@ -99,6 +116,46 @@
 					}
 				}
 			}
+		}
+	}
+
+	::view-transition-old(css-skill-image),
+	::view-transition-new(css-skill-image),
+	::view-transition-old(gsap-skill-image),
+	::view-transition-new(gsap-skill-image) {
+		animation: none;
+		mix-blend-mode: normal;
+	}
+
+	::view-transition-old(css-skill-image),
+	::view-transition-old(gsap-skill-image) {
+		animation: scale-fade-out 2s ease-out forwards;
+	}
+
+	::view-transition-new(css-skill-image),
+	::view-transition-new(gsap-skill-image) {
+		animation: scale-fade-in 1s ease-in forwards;
+	}
+
+	@keyframes scale-fade-out {
+		from {
+			opacity: 1;
+			transform: scale(1);
+		}
+		to {
+			opacity: 0;
+			transform: scale(0.8);
+		}
+	}
+
+	@keyframes scale-fade-in {
+		from {
+			opacity: 0;
+			transform: scale(1.2);
+		}
+		to {
+			opacity: 1;
+			transform: scale(1);
 		}
 	}
 </style>
