c064960007d67ad499d631c2a80cce1999737198
gabrielatwell1987 <gabrielatwell@proton.me>
2025-02-26 12:06:36 -0800

    fixed the opacity for the video on Intro

--- src/lib/components/Burger.svelte
+++ src/lib/components/Burger.svelte
@@ -24,7 +24,7 @@
 			>
 				<path
 					d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"
-					stroke="var(--text-gray)"
+					stroke="var(--text-color)"
 					stroke-width="10"
 				/>
 			</svg>
@@ -37,7 +37,7 @@
 			>
 				<path
 					d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"
-					stroke="var(--text-gray)"
+					stroke="var(--text-color)"
 					stroke-width="10"
 				/>
 			</svg>
@@ -69,13 +69,13 @@
 		}
 
 		svg {
-			color: var(--text-gray);
+			color: var(--text-color);
 			transition: transform 0.2s ease;
 			position: absolute;
 			top: 0;
 
 			&.times {
-				color: var(--text-gray);
+				color: var(--text-color);
 				width: 100%;
 				height: auto;
 			}
