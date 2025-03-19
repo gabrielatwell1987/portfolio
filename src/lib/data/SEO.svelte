978d1a31de06f9981891aba7d6cbe0201821fb59
gabrielatwell1987 <gabrielatwell@proton.me>
2025-03-19 13:20:07 -0700

    fixed my SEO component to have open graph and twitter meta tags

--- src/lib/data/SEO.svelte
+++ src/lib/data/SEO.svelte
@@ -2,6 +2,53 @@
 	import { page } from '$app/stores';
 
 	/** @type {{title: any, description: any, keywords: any}} */
+	let {
+		title = 'Gabriel Atwell',
+		description = 'Web developer and web designer',
+		keywords = ''
+	} = $props();
+
+	let url = $derived($page.url.href);
+	let siteName = 'Atwell UI';
+	let baseUrl = $derived($page.url.origin);
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+
+	<meta name="description" content={description} />
+	<meta name="keywords" content={keywords} />
+
+	<meta name="robots" content="index, follow" />
+	<meta property="og:locale" content="en_US" />
+
+	<meta property="og:title" content={title} />
+	<meta property="og:description" content={description} />
+	<meta property="og:url" content={url} />
+	<meta property="og:type" content="website" />
+	<meta property="og:site_name" content={siteName} />
+	<meta property="og:image" content="{baseUrl}/logos/atwellUI.webp" />
+
+	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:title" content={title} />
+	<meta name="twitter:description" content={description} />
+	<meta name="twitter:image" content="{baseUrl}/logos/atwellUI.webp" />
+
+	<script type="application/ld+json">
+		{JSON.stringify({
+			"@context": "http://schema.org",
+			"@type": "WebSite",
+			"name": "Gabriel Atwell",
+          	"description": "The portfolio of Gabriel Atwell, a web developer and designer based in Las Vegas, NV.",
+			"url": url
+		})}
+	</script>
+</svelte:head>
+
+<!-- <script>
+	import { page } from '$app/stores';
+
+	/** @type {{title: any, description: any, keywords: any}} */
 	let { title, description, keywords } = $props();
 
 	let url = $derived($page.url.href);
@@ -22,4 +69,4 @@
 		  	"url": "https://gabrielatwell.com",
 		}
 	</script>
-</svelte:head>
+</svelte:head> -->
