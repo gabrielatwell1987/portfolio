4f4eed2a3f8b24b534a5a40ca523ba603d2e1727
gabrielatwell1987 <gabrielatwell@proton.me>
2025-03-22 12:13:36 -0700

    made a small change to a project name

--- src/lib/data/SEO.svelte
+++ src/lib/data/SEO.svelte
@@ -44,29 +44,3 @@
 		})}
 	</script>
 </svelte:head>
-
-<!-- <script>
-	import { page } from '$app/stores';
-
-	/** @type {{title: any, description: any, keywords: any}} */
-	let { title, description, keywords } = $props();
-
-	let url = $derived($page.url.href);
-</script>
-
-<svelte:head>
-	<title>{title}</title>
-
-	<meta name="description" content={description} />
-	<meta name="keywords" content={keywords} />
-
-	<script type="application/ld+json">
-		{
-			"@context": "http://schema.org",
-			"@type": "WebSite",
-		  	"name": "Gabriel Atwell",
-          	"description": "The portfolio of Gabriel Atwell, a web developer and designer based in Las Vegas, NV.",
-		  	"url": "https://gabrielatwell.com",
-		}
-	</script>
-</svelte:head> -->
