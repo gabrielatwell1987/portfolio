de2ae8bd0595dcc9849bab678c64b5fdf7c61a71
gabrielatwell1987 <gabrielatwell@proton.me>
2025-04-30 11:18:36 -0700

    made the lines thicker and spaced evenly on LoadingSkeleton

--- src/lib/components/layout/LoadingSkeleton.svelte
+++ src/lib/components/layout/LoadingSkeleton.svelte
@@ -17,11 +17,11 @@
 	></rect>
 	<defs>
 		<clipPath id="clip-path">
-			<rect x="48" y="8" rx="3" ry="3" width="88" height="6" />
-			<rect x="48" y="26" rx="3" ry="3" width="52" height="6" />
-			<rect x="0" y="56" rx="3" ry="3" width="410" height="6" />
-			<rect x="0" y="72" rx="3" ry="3" width="380" height="6" />
-			<rect x="0" y="88" rx="3" ry="3" width="178" height="6" />
+			<rect x="48" y="5" rx="3" ry="3" width="88" height="13" />
+			<rect x="48" y="24" rx="3" ry="3" width="52" height="13" />
+			<rect x="0" y="56" rx="3" ry="3" width="410" height="13" />
+			<rect x="0" y="75" rx="3" ry="3" width="380" height="13" />
+			<rect x="0" y="94" rx="3" ry="3" width="178" height="13" />
 			<circle cx="20" cy="20" r="20" />
 		</clipPath>
 		<linearGradient id="fill">
