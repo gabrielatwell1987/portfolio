071c649a4de70f8e03b98a4163d8519c36fbf738
gabrielatwell1987 <gabrielatwell@proton.me>
2025-05-10 14:28:04 -0700

    i created HireForm and added it to /hire, and linked it on NavBar

---
+++ src/routes/hire/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+	import SEO from '$lib/data/SEO.svelte';
+	import HireForm from '$lib/components/hire/HireForm.svelte';
+	import Title from '$lib/components/layout/Title.svelte';
+</script>
+
+<SEO
+	title="Hire Gabriel Atwell"
+	description="Hire Gabriel Atwell"
+	keywords="hire gabriel atwell, project questionnaire"
+/>
+
+<Title title="hire me" />
+
+<section>
+	<HireForm />
+</section>
+
+<style>
+	section {
+		display: grid;
+		place-items: center;
+		height: 100vh;
+	}
+</style>
