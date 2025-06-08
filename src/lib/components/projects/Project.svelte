621d50085b624452c3cfe6b08248463290ef4c7b
gabrielatwell1987 <gabrielatwell@proton.me>
2025-06-08 12:40:56 -0700

    changed the font-weight of .summary on Project

--- src/lib/components/projects/Project.svelte
+++ src/lib/components/projects/Project.svelte
@@ -32,6 +32,7 @@
 			width: 75%;
 			margin-inline: auto;
 			font-size: clamp(var(--sm), 1.15vw, var(--h5));
+			font-weight: 500;
 			color: var(--clr-pale);
 			margin-top: 1rem;
 			text-align: center;
