14a8d09c03fef22419a2fba7474558baec82794e
gabrielatwell1987 <gabrielatwell@proton.me>
2025-06-11 13:34:42 -0700

    on mobile i made the font sizing bigger on /about and Grid

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -139,7 +139,7 @@
 					& .text {
 						max-width: var(--100);
 						margin-inline: auto;
-						font-size: clamp(var(--sm), 1.5vw, var(--h4));
+						font-size: clamp(var(--h6), 1.5vw, var(--h4));
 						font-weight: 500;
 						line-height: 1.5;
 						text-wrap: none;
