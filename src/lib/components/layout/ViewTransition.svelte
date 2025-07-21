53650b51276436717c0c504368f268b9f487bf18
gabrielatwell1987 <gabrielatwell@proton.me>
2025-07-20 17:20:21 -0700

    made ViewTransition default transitions

--- src/lib/components/layout/ViewTransition.svelte
+++ src/lib/components/layout/ViewTransition.svelte
@@ -12,69 +12,7 @@
 </script>
 
 <style>
-	:root {
-		background-color: #000;
-	}
-
-	:root::view-transition-old(root) {
-		animation: 0.75s cubic-bezier(0.37, 0.06, 0.6, 0.91) both out;
-		animation-delay: 0.5s;
-	}
-
-	:root::view-transition-new(root) {
-		animation: 0.75s cubic-bezier(0.37, 0.06, 0.6, 0.91) 1s both in;
-		animation-delay: 0.5s;
-	}
-
-	@supports (-webkit-touch-callout: none) {
-		:root::view-transition-old(root) {
-			animation: 0.25s cubic-bezier(0.4, 0, 1, 1) both out-safari;
-			animation-delay: 0.5s;
-		}
-
-		:root::view-transition-new(root) {
-			animation: 0.75s cubic-bezier(0, 0, 0.2, 1) both in-safari;
-			animation-delay: 0.5s;
-		}
-	}
-
-	@keyframes in {
-		from {
-			opacity: 0.3;
-		}
-
-		to {
-			opacity: 1;
-		}
-	}
-
-	@keyframes out {
-		from {
-			opacity: 1;
-		}
-
-		to {
-			opacity: 0.3;
-		}
-	}
-
-	@keyframes in-safari {
-		from {
-			opacity: 0.65;
-		}
-
-		to {
-			opacity: 1;
-		}
-	}
-
-	@keyframes out-safari {
-		from {
-			opacity: 1;
-		}
-
-		to {
-			opacity: 0.65;
-		}
+	@view-transition {
+		navigation: auto;
 	}
 </style>
