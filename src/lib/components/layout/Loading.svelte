18fe7bd879c9aac3274dd003299cc38a3e7d5a61
gabrielatwell1987 <gabrielatwell@proton.me>
2025-07-26 11:35:36 -0700

    the Loading screen was causing the CLS

--- src/lib/components/layout/Loading.svelte
+++ src/lib/components/layout/Loading.svelte
@@ -8,9 +8,15 @@
 
 <style>
 	.container {
+		position: fixed;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		width: 100%;
 		height: 100vh;
 		display: flex;
 		justify-content: center;
 		align-items: center;
+		z-index: 9999;
 	}
 </style>
