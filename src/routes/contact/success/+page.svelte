1e699223c05877488fb42b6d958309d358399205
gabrielatwell1987 <gabrielatwell@proton.me>
2025-09-14 16:41:58 -0700

    fixed the success page spacing.. changed Image to img

--- src/routes/contact/success/+page.svelte
+++ src/routes/contact/success/+page.svelte
@@ -11,13 +11,13 @@
 
 <main>
 	<section>
-		<div class="image">
-			<Image src="/logos/success.webp" alt="Atwell" width="500" aspectRatio="16/9" />
+		<img src="/logos/success.webp" alt="Atwell" width="500" />
+
+		<div class="text-content">
+			<p class="small"><i>Your message has been sent.</i></p>
+
+			<p>Thank you for reaching out!</p>
 		</div>
-
-		<p class="small"><i>Your message has been sent.</i></p>
-
-		<p>Thank you for reaching out!</p>
 	</section>
 </main>
 
@@ -30,33 +30,32 @@
 
 		main {
 			min-height: 70vh;
+			padding-top: 7em;
 
 			& section {
 				width: var(--100);
-				padding: clamp(2rem, 5vw, 9rem);
+				padding: clamp(2rem, 5vw, 5em);
 				margin-inline: auto;
 				margin-top: var(--5);
 				text-align: center;
 			}
 
-			& p {
-				font-size: clamp(var(--sm), 1.5vw, var(--h4));
-				font-weight: 500;
-				margin: 0.5em 0;
-				color: var(--clr-blue);
-			}
+			& .text-content {
+				padding-top: 3em;
 
-			& .small {
-				font-size: clamp(var(--xs), 1.2vw, var(--h6));
-				font-weight: 200;
-				margin: 2em 0 -0.5rem 0;
-				color: var(--clr-gray);
-			}
+				& p {
+					font-size: clamp(var(--sm), 1.5vw, var(--h4));
+					font-weight: 500;
+					margin: 0.5em 0;
+					color: var(--clr-blue);
+				}
 
-			& .image {
-				width: clamp(13em, 70vw, 15in);
-				margin-inline: auto;
-				margin-bottom: clamp(2.5rem, 3vw, 5rem);
+				& .small {
+					font-size: clamp(var(--xs), 1.2vw, var(--h6));
+					font-weight: 200;
+					margin: 0.5em 0 -0.5rem 0;
+					color: var(--clr-gray);
+				}
 			}
 		}
 	}
