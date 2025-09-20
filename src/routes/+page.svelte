2592d56b236a41988c4ee915eb02db23cfefe661
gabrielatwell1987 <gabrielatwell@proton.me>
2025-09-20 13:09:27 -0700

    changed the theming a little bit and added DoYouNeed to Hero, deleted Intro and /landing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
-	import Intro from '$lib/components/landing/Intro.svelte';
 	import SEO from '$lib/data/SEO.svelte';
+	import Hero from '$lib/components/landing/Hero.svelte';
 </script>
 
 <SEO
@@ -9,4 +9,4 @@
 	keywords="gabriel atwell frontend developer, gabe atwell frontend developer, gabriel atwell web designer, gabe atwell web designer, atwell design, atwell ui, gabriel atwell portfolio, gabe atwell portfolio, las vegas web designer, las vegas web developer, responsive website development, user-centered ui/ux design, custom web design services"
 />
 
-<Intro />
+<Hero />
