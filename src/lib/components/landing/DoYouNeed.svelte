d3175bb785b7c60cdad179073ee33ed33f4ec178
gabrielatwell1987 <gabrielatwell@proton.me>
2025-09-21 14:06:37 -0700

    lowered the size of .bigWord and nested the css

--- src/lib/components/landing/DoYouNeed.svelte
+++ src/lib/components/landing/DoYouNeed.svelte
@@ -27,87 +27,87 @@
 </section>
 
 <style>
-	.sentence,
-	.bigWord {
-		-webkit-font-smoothing: antialiased;
-		-moz-osx-font-smoothing: grayscale;
-	}
-
 	.animated-text {
 		padding-top: 10em;
 
 		@media (width <= 768px) {
 			margin-top: -15%;
 		}
-	}
 
-	.sentence {
-		font-family: var(--bronova);
-		font-size: clamp(1.25rem, 2.5vw, 10rem);
-		font-weight: 100;
-		color: var(--clr-main);
-		text-align: center;
-		margin: 0;
-		padding: 2rem;
-		transform: translateY(100%);
-		opacity: 0;
-		transition:
-			transform 1.25s ease-in-out,
-			opacity 1.25s ease-in-out;
-		transition-delay: 1s;
-
-		@media (width <= 768px) {
-			margin-top: 7%;
-			opacity: 0.9;
-		}
-
-		&.animate {
-			transform: translateY(0);
-			opacity: 1;
-		}
-	}
-
-	.bigWord {
-		font-family: var(--ultra);
-		font-size: clamp(2.5rem, 10vw, 35rem);
-		text-wrap: none;
-		display: block;
-		letter-spacing: -1px;
-		line-height: 1.2;
-
-		@media (width <= 768px) {
-			font-weight: 600;
-			margin-top: 2%;
-		}
-
-		& .char {
-			display: inline-block;
-			transform: translateX(150%);
+		.sentence {
+			font-family: var(--bronova);
+			font-size: clamp(1.25rem, 2.5vw, 10rem);
+			font-weight: 100;
+			color: var(--clr-main);
+			text-align: center;
+			margin: 0;
+			padding: 2rem;
+			transform: translateY(100%);
 			opacity: 0;
-			animation: charAnimation 3s cubic-bezier(0.215, 0.61, 0.355, 1) forwards;
-			animation-delay: calc(var(--delay) + 2.25s);
+			transition:
+				transform 1.25s ease-in-out,
+				opacity 1.25s ease-in-out;
+			transition-delay: 1s;
+
+			@media (width <= 768px) {
+				margin-top: 7%;
+				opacity: 0.9;
+			}
+
+			&.animate {
+				transform: translateY(0);
+				opacity: 1;
+			}
+
+			.bigWord {
+				font-family: var(--ultra);
+				font-size: clamp(2.5rem, 8vw, 35rem);
+				text-wrap: none;
+				display: block;
+				letter-spacing: -1px;
+				line-height: 1.2;
+
+				@media (width <= 768px) {
+					font-weight: 600;
+					margin-top: 2%;
+				}
+
+				.char {
+					display: inline-block;
+					transform: translateX(150%);
+					opacity: 0;
+					animation: charAnimation 3s cubic-bezier(0.215, 0.61, 0.355, 1) forwards;
+					animation-delay: calc(var(--delay) + 2.25s);
+				}
+			}
+
+			.glow {
+				color: var(--clr-inverted);
+				text-shadow:
+					0 0 5px #17262c,
+					0 0 5px var(--clr-main),
+					0 0 5px var(--clr-main),
+					0 0 5px var(--clr-main),
+					0 0 6px var(--clr-main),
+					0 0 7px var(--clr-main);
+
+				@media (width <= 768px) {
+					text-shadow:
+						0 0 3px #17262c,
+						0 0 4px #eee,
+						0 0 4px #eee,
+						0 0 4px #eee,
+						0 0 4px #eee,
+						0 0 4px #eee;
+				}
+			}
 		}
 	}
 
-	.glow {
-		color: var(--clr-inverted);
-		text-shadow:
-			0 0 5px #17262c,
-			0 0 5px var(--clr-main),
-			0 0 5px var(--clr-main),
-			0 0 5px var(--clr-main),
-			0 0 6px var(--clr-main),
-			0 0 7px var(--clr-main);
-
-		@media (width <= 768px) {
-			text-shadow:
-				0 0 3px #17262c,
-				0 0 4px #eee,
-				0 0 4px #eee,
-				0 0 4px #eee,
-				0 0 4px #eee,
-				0 0 4px #eee;
-		}
+	.sentence,
+	.bigWord {
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
 	}
 
 	@keyframes charAnimation {
