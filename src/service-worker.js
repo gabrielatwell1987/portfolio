145d61003c9fa8494bb36a6252864d36775a730d
gabrielatwell1987 <gabrielatwell@proton.me>
2025-09-26 12:43:24 -0700

    fixed service worker errors

--- src/service-worker.js
+++ src/service-worker.js
@@ -74,10 +74,3 @@ self.addEventListener('message', (event) => {
 		self.skipWaiting();
 	}
 });
-
-// if ('serviceWorker' in navigator) {
-// 	navigator.serviceWorker
-// 		.register('/service-worker.js')
-// 		.then(() => console.log('Service Worker registered'))
-// 		.catch((err) => console.error('Service Worker registration failed:', err));
-// }
