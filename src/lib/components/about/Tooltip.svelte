0bc14a1d9bafb05074f86e2fcb4553663e19af38
gabrielatwell1987 <gabrielatwell@proton.me>
2025-10-02 16:29:42 -0700

    fixed Tooltip for mobile

--- src/lib/components/about/Tooltip.svelte
+++ src/lib/components/about/Tooltip.svelte
@@ -5,7 +5,7 @@
 
 <main class="tooltip-container" aria-label="tooltip container">
 	<span class="tooltip-trigger">
-		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -10 384 530">
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 530">
 			<title>Attention!</title>
 			<path
 				d="M272 384c9.6-31.9 29.5-59.1 49.2-86.2c0 0 0 0 0 0c5.2-7.1 10.4-14.2 15.4-21.4c19.8-28.5 31.4-63 31.4-100.3C368 78.8 289.2 0 192 0S16 78.8 16 176c0 37.3 11.6 71.9 31.4 100.3c5 7.2 10.2 14.3 15.4 21.4c0 0 0 0 0 0c19.8 27.1 39.7 54.4 49.2 86.2l160 0zM192 512c44.2 0 80-35.8 80-80l0-16-160 0 0 16c0 44.2 35.8 80 80 80zM112 176c0 8.8-7.2 16-16 16s-16-7.2-16-16c0-61.9 50.1-112 112-112c8.8 0 16 7.2 16 16s-7.2 16-16 16c-44.2 0-80 35.8-80 80z"
@@ -45,9 +45,9 @@
 				fill: var(--clr-blue);
 				stroke: var(--clr-main);
 				stroke-width: calc(5px + 1vw);
-
-				width: clamp(0.85em, 5vw, 3em);
-				height: clamp(0.85em, 5vw, 3em);
+				width: clamp(1.2em, 5vw, 3em);
+				height: clamp(1.2em, 5vw, 3em);
+				overflow: visible;
 
 				@media (width <= 768px) {
 					stroke-width: calc(5px + 5vw);
@@ -61,15 +61,15 @@
 
 		& .tooltip {
 			position: absolute;
-			background-color: var(--black);
-			color: var(---white);
+			background-color: var(--clr-inverted);
+			color: var(-clr-main);
 			font-size: clamp(var(--sm), 1.5vw, var(--h6));
 			padding: 0.5rem;
-			box-shadow: 0 0 5px var(--clr-blue);
+			box-shadow: 0 0 3px var(--clr-blue);
 			border-radius: 16px;
 			max-inline-size: 200px;
 			width: fit-content;
-			z-index: 1;
+			z-index: 10;
 			opacity: 0;
 			visibility: hidden;
 			transition: all 1s ease;
@@ -89,11 +89,19 @@
 			transform: translateX(-50%);
 			transition: transform 0.25s ease;
 
+			@media (width <= 768px) {
+				transform: translateX(-60%);
+			}
+
 			&::after {
 				top: 100%;
 				left: 50%;
 				margin-left: -5px;
 				border-color: var(--clr-gray) transparent transparent transparent;
+
+				@media (width <= 768px) {
+					left: 60%;
+				}
 			}
 		}
 
