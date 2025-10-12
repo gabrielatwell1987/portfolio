cfeb50789000a1f9bb6f862f2297bea9c75c9728
gabrielatwell1987 <gabrielatwell@proton.me>
2025-10-12 12:50:59 -0700

    fixed transition change and made gsap.md for the docs folder

--- src/lib/components/layout/TransitionChange.svelte
+++ src/lib/components/layout/TransitionChange.svelte
@@ -2,20 +2,26 @@
 	import gsap from 'gsap';
 	import { MorphSVGPlugin } from 'gsap/MorphSVGPlugin';
 
-	let { fillColor = 'rgba(0, 0, 0, 0)', endColor } = $props();
+	// let { fillColor, endColor } = $props();
+	let svgElement;
 
 	$effect(() => {
 		gsap.registerPlugin(MorphSVGPlugin);
 
-		const tl = gsap
-			.timeline()
-			.to(['#star-path', '#visible-star'], {
-				morphSVG: 'M 50 5 L 61 35 L 95 35 L 68 54 L 79 85 L 50 66 L 21 85 L 32 54 L 5 35 L 39 35 Z',
-				duration: 1,
-				scale: 7,
-				ease: 'power2.out'
-			})
-			.to('#demo', { display: 'none', duration: 0.05, ease: 'power1.inOut' }, '-=.25');
+		const tl = gsap.timeline({
+			repeat: 1,
+			yoyo: true
+		});
+
+		tl.to('.shape1', {
+			duration: 0.5,
+			morphSVG: '.shape2',
+			onComplete: () => {
+				gsap.delayedCall(1.25, () => {
+					svgElement.style.display = 'none';
+				});
+			}
+		});
 
 		return () => {
 			tl.kill();
@@ -23,28 +29,62 @@
 	});
 </script>
 
-<svg id="demo" class="overlay overlay--2" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-	<defs>
-		<mask id="star-mask">
-			<rect width="100" height="100" fill="white" />
-			<path id="star-path" d="M 50 50 L 50 50 Z" fill={fillColor} />
-		</mask>
-	</defs>
-	<rect width="100" height="100" fill="rgb({endColor})" mask="url(#star-mask)" />
-
-	<path id="visible-star" d="M 50 50 L 50 50 Z" fill="none" stroke="black" stroke-width="2" />
-</svg>
+<div class="overlay">
+	<svg
+		bind:this={svgElement}
+		id="demo"
+		viewBox="0 -200 500 950"
+		xmlns="http://www.w3.org/2000/svg"
+		xmlns:xlink="http://www.w3.org/1999/xlink"
+		xmlns:bx="https://boxy-svg.com"
+	>
+		<defs>
+			<path
+				class="shape2"
+				d="M 246.977 47.68 L 433.221 182.995 L 362.082 401.938 L 131.872 401.938 L 60.733 182.995 Z"
+			/>
+			<mask id="mask-0">
+				<path
+					class="shape1"
+					d="M 260.519 25.987 L 315.838 185.131 L 484.287 188.564 L 350.026 290.354 L 398.815 451.62 L 260.519 355.385 L 122.223 451.62 L 171.012 290.354 L 36.751 188.564 L 205.2 185.131 Z"
+					fill="#ffffff"
+				/>
+			</mask>
+		</defs>
+		<image
+			width="500"
+			height="800"
+			x="10"
+			y="-150"
+			style="mask: url(#mask-0);"
+			xlink:href="/logos/a-dev.webp"
+		/>
+	</svg>
+</div>
 
 <style>
 	* {
 		position: relative;
 	}
 
-	#demo {
-		background: transparent;
-		width: 100%;
-		height: 100%;
-		overflow: hidden;
-		z-index: 1;
+	.overlay {
+		position: fixed;
+		inset: 0;
+		width: 100vw;
+		height: 100vh;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		z-index: 10;
+		pointer-events: none;
+
+		#demo {
+			background: transparent;
+			height: 100%;
+			overflow: visible;
+			z-index: 1;
+			width: 60dvh;
+			height: auto;
+		}
 	}
 </style>
