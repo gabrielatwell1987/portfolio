6f730af78b715bca930f438bc87a07cfed40115f
gabrielatwell1987 <gabrielatwell@proton.me>
2025-10-13 13:49:13 -0700

    fixed TransitionChange

--- src/lib/components/layout/TransitionChange.svelte
+++ src/lib/components/layout/TransitionChange.svelte
@@ -2,20 +2,17 @@
 	import gsap from 'gsap';
 	import { MorphSVGPlugin } from 'gsap/MorphSVGPlugin';
 
-	// let { fillColor, endColor } = $props();
 	let svgElement;
 
 	$effect(() => {
 		gsap.registerPlugin(MorphSVGPlugin);
 
-		const tl = gsap.timeline({
-			repeat: 1,
-			yoyo: true
-		});
+		const tl = gsap.timeline();
 
 		tl.to('.shape1', {
 			duration: 0.5,
 			morphSVG: '.shape2',
+			transformOrigin: '50% 50%',
 			onComplete: () => {
 				gsap.delayedCall(1, () => {
 					svgElement.style.display = 'none';
@@ -33,31 +30,25 @@
 	<svg
 		bind:this={svgElement}
 		id="demo"
-		viewBox="0 -200 500 950"
 		xmlns="http://www.w3.org/2000/svg"
 		xmlns:xlink="http://www.w3.org/1999/xlink"
-		xmlns:bx="https://boxy-svg.com"
+		viewBox="0 0 100 100"
 	>
 		<defs>
-			<path
-				class="shape2"
-				d="M 246.977 47.68 L 433.221 182.995 L 362.082 401.938 L 131.872 401.938 L 60.733 182.995 Z"
-			/>
-			<mask id="mask-0">
-				<path
-					class="shape1"
-					d="M 260.519 25.987 L 315.838 185.131 L 484.287 188.564 L 350.026 290.354 L 398.815 451.62 L 260.519 355.385 L 122.223 451.62 L 171.012 290.354 L 36.751 188.564 L 205.2 185.131 Z"
-					fill="#ffffff"
-				/>
-			</mask>
+			<radialGradient id="gradient-fill" cx="50%" cy="50%" r="50%">
+				<stop offset="0%" style="stop-color:var(--clr-link);stop-opacity:1" />
+				<stop offset="100%" style="stop-color:var(--clr-main);stop-opacity:1" />
+			</radialGradient>
+			<path class="shape2" d="M 50,15 L 85,50 L 50,85 L 15,50 Z" />
 		</defs>
-		<image
-			width="500"
-			height="800"
-			x="10"
-			y="-150"
-			style="mask: url(#mask-0);"
-			xlink:href="/logos/a-dev.webp"
+
+		<path
+			class="shape1"
+			d="M 50,16 L 58,44 L 84,50 L 58,56 L 50,84 L 42,56 L 16,50 L 42,44 Z"
+			fill="url(#gradient-fill)"
+			stroke="#000"
+			stroke-width="2"
+			stroke-linejoin="round"
 		/>
 	</svg>
 </div>
@@ -80,7 +71,7 @@
 
 		#demo {
 			background: transparent;
-			height: 100%;
+			/* height: 100%; */
 			overflow: visible;
 			z-index: 1;
 			width: 60dvh;
