e33c983850204bd2e6d8f7bdee8930b1fd171ad2
gabrielatwell1987 <gabrielatwell@proton.me>
2025-10-14 12:15:35 -0700

    changed TransitionChange to StarMorph and made it look better as an intro animation

--- src/lib/components/layout/StarMorph.svelte
+++ src/lib/components/layout/StarMorph.svelte
@@ -14,7 +14,7 @@
 			morphSVG: '.shape2',
 			transformOrigin: '50% 50%',
 			onComplete: () => {
-				gsap.delayedCall(1, () => {
+				gsap.delayedCall(0.55, () => {
 					svgElement.style.display = 'none';
 				});
 			}
@@ -26,9 +26,8 @@
 	});
 </script>
 
-<div class="overlay">
+<div class="overlay" bind:this={svgElement}>
 	<svg
-		bind:this={svgElement}
 		id="demo"
 		xmlns="http://www.w3.org/2000/svg"
 		xmlns:xlink="http://www.w3.org/1999/xlink"
@@ -36,15 +35,15 @@
 	>
 		<defs>
 			<radialGradient id="gradient-fill" cx="50%" cy="50%" r="50%">
-				<stop offset="0%" style="stop-color:var(--clr-link);stop-opacity:1" />
+				<stop offset="0%" style="stop-color:var(--clr-gray);stop-opacity:1" />
 				<stop offset="100%" style="stop-color:var(--clr-main);stop-opacity:1" />
 			</radialGradient>
-			<path class="shape2" d="M 50,15 L 85,50 L 50,85 L 15,50 Z" />
+			<path class="shape2" d="M 50,15 L 67,32 L 85,50 L 67,68 L 50,85 L 33,68 L 15,50 L 33,32 Z" />
 		</defs>
 
 		<path
 			class="shape1"
-			d="M 50,16 L 58,44 L 84,50 L 58,56 L 50,84 L 42,56 L 16,50 L 42,44 Z"
+			d="M 50,10 L 48,48 L 10,50 L 48,52 L 50,90 L 52,52 L 90,50 L 52,48 Z"
 			fill="url(#gradient-fill)"
 			stroke="#000"
 			stroke-width="2"
@@ -66,12 +65,19 @@
 		display: flex;
 		justify-content: center;
 		align-items: center;
-		z-index: 10;
+		z-index: 1;
 		pointer-events: none;
+		background-image: radial-gradient(
+			circle,
+			transparent 0%,
+			transparent 46%,
+			var(--darker-blue) 47%,
+			#000 60%,
+			#000 100%
+		);
 
 		#demo {
 			background: transparent;
-			/* height: 100%; */
 			overflow: visible;
 			z-index: 1;
 			width: 60dvh;
