1c1410e8b5ec9961641b2654cb15c1208421721a
gabrielatwell1987 <gabrielatwell@proton.me>
2025-10-16 13:14:00 -0700

    fixed HeroButton theming

--- src/lib/components/landing/HeroButton.svelte
+++ src/lib/components/landing/HeroButton.svelte
@@ -14,19 +14,19 @@
 			font-family: var(--ultra);
 			font-size: clamp(var(--h5), 2vw, var(--h3));
 			font-weight: 900;
-			color: var(--clr-invert);
+			color: var(--clr-main);
 			text-shadow:
-				0 0 2px var(--clr-invert),
-				-2px -2px 0 var(--clr-main),
-				2px -2px 0 var(--clr-main),
-				-2px 2px 0 var(--clr-main),
-				2px 2px 0 var(--clr-main),
-				-2px 0 0 var(--clr-main),
-				2px 0 0 var(--clr-main),
-				0 -2px 0 var(--clr-main),
-				0 2px 0 var(--clr-main);
-			border: 1px solid var(--clr-invert);
-			outline: 3px solid var(--clr-invert);
+				0 0 2px var(--clr-main),
+				-2px -2px 0 var(--clr-invert),
+				2px -2px 0 var(--clr-invert),
+				-2px 2px 0 var(--clr-invert),
+				2px 2px 0 var(--clr-invert),
+				-2px 0 0 var(--clr-invert),
+				2px 0 0 var(--clr-invert),
+				0 -2px 0 var(--clr-invert),
+				0 2px 0 var(--clr-invert);
+			border: 1px solid var(--clr-main);
+			outline: 3px solid var(--clr-main);
 			margin-inline: auto;
 			cursor: pointer;
 			letter-spacing: 1px;
@@ -37,19 +37,9 @@
 			padding: clamp(0.4em, 1.5vw, 0.3em) clamp(1em, 2vw, 0.7em);
 			margin-bottom: 2%;
 			user-select: none;
-			opacity: 0.75;
 
-			@media (width <= 768px) {
-				text-shadow:
-					0 0 1px var(--clr-invert),
-					-1px -1px 0 var(--clr-main),
-					1px -1px 0 var(--clr-main),
-					-1px 1px 0 var(--clr-main),
-					1px 1px 0 var(--clr-main),
-					-1px 0 0 var(--clr-main),
-					1px 0 0 var(--clr-main),
-					0 -1px 0 var(--clr-main),
-					0 1px 0 var(--clr-main);
+			&:not(:hover) {
+				opacity: 0.85;
 			}
 
 			&:focus,
@@ -59,7 +49,6 @@
 			}
 
 			&:hover {
-				opacity: 1;
 				outline-offset: 0px;
 			}
 
