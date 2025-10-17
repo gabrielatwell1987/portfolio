519c937b1e40989971a85cce3e5bb7eeb632190c
gabrielatwell1987 <gabrielatwell@proton.me>
2025-10-17 11:15:28 -0700

    fixed MotifPhoto and use it on /hire

--- src/lib/components/contact/MotifPhoto.svelte
+++ src/lib/components/contact/MotifPhoto.svelte
@@ -1,37 +1,77 @@
 <script>
 	import Image from '$lib/components/layout/Image.svelte';
+
+	function generateBlobPath() {
+		const points = 7;
+		const radius = 200;
+		const variance = 0.3;
+
+		let path = 'M ';
+
+		for (let i = 0; i < points; i++) {
+			const angle = (i / points) * Math.PI * 2;
+			const randomRadius = radius * (1 + (Math.random() - 0.5) * variance);
+			const x = 250 + Math.cos(angle) * randomRadius;
+			const y = 250 + Math.sin(angle) * randomRadius;
+
+			if (i === 0) {
+				path += `${x},${y} `;
+			} else {
+				// Use quadratic curves for smooth blob shape
+				const prevAngle = ((i - 1) / points) * Math.PI * 2;
+				const prevRadius = radius * (1 + (Math.random() - 0.5) * variance);
+				const cpx = 250 + Math.cos(prevAngle + (0.5 / points) * Math.PI * 2) * prevRadius;
+				const cpy = 250 + Math.sin(prevAngle + (0.5 / points) * Math.PI * 2) * prevRadius;
+				path += `Q ${cpx},${cpy} ${x},${y} `;
+			}
+		}
+		path + 'Z';
+	}
+
+	let blobPath = $state(generateBlobPath());
 </script>
 
-<div class="bg"></div>
-<!-- <div class="img"></div> -->
-<Image
-	src="/images/hire-gabe.webp"
-	alt="Gabriel Atwell"
-	style="width: clamp(200px, 30vw, 750px);"
-	aspectRatio="1/1"
-/>
+<div class="blob-wrapper">
+	<svg viewBox="0 0 500 500" class="blob-bg">
+		<path d={blobPath} fill="var(--warning)" />
+	</svg>
+
+	<div class="image-wrapper">
+		<Image
+			src="/images/hire-gabe.webp"
+			alt="Gabriel Atwell"
+			style="width: clamp(200px, 30vw, 750px);"
+			aspectRatio="1/1"
+		/>
+	</div>
+</div>
 
 <style>
-	.bg {
-		background-color: var(--clr-pale);
-		padding: 1rem;
-		border-radius: calc(var(--radius) * 3);
-		box-shadow: 0 0 1px var(--clr-invert);
-		width: 25vw;
-		height: 25vw;
-		rotate: 45deg;
-		margin-inline: auto;
-		opacity: 0.25;
-		overflow: visible;
+	.blob-wrapper {
 		position: relative;
-		top: 400px;
-		/* margin-top: -20em; */
+		width: clamp(200px, 30vw, 750px);
+		margin-inline: auto;
 
-		/* & .img {
-			opacity: 1;
+		& .blob-bg {
 			position: absolute;
-			top: -30px;
+			top: 0;
+			left: 0;
+			filter: drop-shadow(0 0 1px var(--clr-invert));
+			z-index: -1;
 			width: 100%;
-		} */
+			height: 100%;
+		}
+
+		& .image-wrapper {
+			position: relative;
+			width: 100%;
+			height: 100%;
+			border-radius: 50%;
+			overflow: hidden;
+			box-shadow: 0 5px 0 rgba(0, 0, 0, 0.8);
+			border: 1px solid rgba(0, 0, 0, 0.8);
+			z-index: 1;
+			/* background-color: var(--clr-invert); */
+		}
 	}
 </style>
