c7e01b3e85dd137aa5a3ae11b4aadbb6dcb51c5b
gabrielatwell1987 <gabrielatwell@proton.me>
2025-10-22 12:15:41 -0700

    made .card-front brighter in CardFlip

--- src/lib/components/about/CardFlip.svelte
+++ src/lib/components/about/CardFlip.svelte
@@ -44,95 +44,92 @@
 		align-items: flex-start;
 		justify-content: center;
 
-		/* @media (width <= 500px) {
-			margin-bottom: 3em;
-		} */
-	}
+		& .card-container {
+			width: 300px;
+			height: 400px;
+			perspective: 1000px;
+			display: flex;
+			justify-content: center;
+			align-items: center;
 
-	.card-container {
-		width: 300px;
-		height: 400px;
-		perspective: 1000px;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-
-		@media screen and (width <= 600px) {
-			object-fit: contain;
-		}
-
-		@media (hover: hover) {
-			&:hover .card {
-				transform: rotateY(180deg);
-			}
-		}
-
-		.card {
-			position: relative;
-			width: 100%;
-			height: 100%;
-			text-align: center;
-			transition: transform 0.7s;
-			transform-style: preserve-3d;
-			cursor: pointer;
-			border: none;
-			background: transparent;
-			padding: 0;
-			margin: 0;
-			display: block;
-			box-sizing: border-box;
-
-			&.flipped {
-				transform: rotateY(180deg);
+			@media screen and (width <= 600px) {
+				object-fit: contain;
 			}
 
-			&:focus {
-				outline: 2px solid var(--clr-main);
-				outline-offset: 2px;
-			}
-
-			.card-front,
-			.card-back {
-				position: absolute;
-				width: 100%;
-				height: 100%;
-				-webkit-backface-visibility: hidden;
-				backface-visibility: hidden;
-				display: flex;
-				align-items: center;
-				justify-content: center;
-				border-radius: 10px;
-
-				img {
-					object-fit: cover;
-					width: 100%;
-					height: 100%;
-					position: absolute;
-					top: 0;
-					left: 0;
-					filter: drop-shadow(0 0 0.25rem var(--clr-main));
-					border-radius: 10px;
-					aspect-ratio: 3 / 4;
-
-					@media (width <= 500px) {
-						width: 85vw;
-						height: auto;
-						object-fit: contain;
-					}
+			@media (hover: hover) {
+				&:hover .card {
+					transform: rotateY(180deg);
 				}
 			}
 
-			.card-front {
-				background-color: transparent;
-				box-shadow: none;
-				color: var(--white);
-			}
+			& .card {
+				position: relative;
+				width: 100%;
+				height: 100%;
+				text-align: center;
+				transition: transform 0.7s;
+				transform-style: preserve-3d;
+				cursor: pointer;
+				border: none;
+				background: transparent;
+				padding: 0;
+				margin: 0;
+				display: block;
+				box-sizing: border-box;
 
-			.card-back {
-				background-color: transparent;
-				box-shadow: none;
-				color: var(--white);
-				transform: rotateY(180deg);
+				&.flipped {
+					transform: rotateY(180deg);
+				}
+
+				&:focus {
+					outline: 2px solid var(--clr-main);
+					outline-offset: 2px;
+				}
+
+				& .card-front,
+				& .card-back {
+					position: absolute;
+					width: 100%;
+					height: 100%;
+					-webkit-backface-visibility: hidden;
+					backface-visibility: hidden;
+					display: flex;
+					align-items: center;
+					justify-content: center;
+					border-radius: 10px;
+
+					& img {
+						object-fit: cover;
+						width: 100%;
+						height: 100%;
+						position: absolute;
+						top: 0;
+						left: 0;
+						filter: drop-shadow(0 0 0.25rem var(--clr-main));
+						border-radius: 10px;
+						aspect-ratio: 3 / 4;
+
+						@media (width <= 500px) {
+							width: 85vw;
+							height: auto;
+							object-fit: contain;
+						}
+					}
+				}
+
+				& .card-front {
+					background-color: transparent;
+					box-shadow: none;
+					color: var(--white);
+					filter: brightness(150%);
+				}
+
+				& .card-back {
+					background-color: transparent;
+					box-shadow: none;
+					color: var(--white);
+					transform: rotateY(180deg);
+				}
 			}
 		}
 	}
