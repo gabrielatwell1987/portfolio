b1163001ce32e4971d09b89204c2b3f8bf903139
gabrielatwell1987 <gabrielatwell@proton.me>
2025-11-08 12:19:54 -0800

    added hire-logo.svg and used it to make an overlay on MotifPhoto

--- src/lib/components/contact/MotifPhoto.svelte
+++ src/lib/components/contact/MotifPhoto.svelte
@@ -1,14 +1,59 @@
 <script>
 </script>
 
-<img src="/images/gabe.webp" alt="gabe atwell" />
+<div class="hire-wrapper">
+	<img src="/images/gabe.webp" alt="gabe atwell" />
+	<img src="/logos/hire-logo.svg" alt="hire svg" class="hire-logo" />
+</div>
 
 <style>
-	img {
-		width: clamp(200px, 30vw, 750px);
-		height: clamp(200px, 30vw, 750px);
-		border-radius: 10% var(--radius);
-		display: block;
+	.hire-wrapper {
+		position: relative;
+		width: fit-content;
 		margin-inline: auto;
+
+		& img {
+			width: clamp(200px, 30vw, 750px);
+			height: clamp(200px, 30vw, 750px);
+			border-radius: 10% var(--radius);
+			display: block;
+			margin-inline: auto;
+			z-index: 1;
+		}
+
+		& .hire-logo {
+			position: absolute;
+			bottom: 4vw;
+			right: 1vw;
+			width: 100px;
+			height: auto;
+			z-index: 5;
+			scale: 8;
+
+			@media (width <= 1400px) {
+				scale: 7;
+			}
+
+			@media (width <= 1100px) {
+				scale: 5;
+				bottom: 2.5vw;
+				right: 0;
+			}
+
+			@media (width <= 990px) {
+				right: -1vw;
+			}
+
+			@media (width <= 768px) {
+				scale: 3;
+				bottom: 0;
+				right: -3vw;
+			}
+
+			@media (width <= 500px) {
+				right: -6vw;
+				bottom: 1vw;
+			}
+		}
 	}
 </style>
--- src/lib/components/learn/UsefulLinks.svelte
+++ src/lib/components/learn/UsefulLinks.svelte
@@ -77,17 +77,17 @@
 				font-family: var(--bronova);
 				font-size: clamp(var(--h6), 2vw, var(--h4));
 				/* font-weight: 900; */
-				color: var(--clr-blue);
+				color: var(--clr-invert);
 				text-shadow:
-					0 0 1px var(--clr-invert),
-					-1px -1px 0 var(--clr-blue),
-					1px -1px 0 var(--clr-blue),
-					-1px 1px 0 var(--clr-blue),
-					1px 1px 0 var(--clr-blue),
-					-1px 0 0 var(--clr-blue),
-					1px 0 0 var(--clr-blue),
-					0 -1px 0 var(--clr-blue),
-					0 1px 0 var(--clr-blue);
+					0 0 2px var(--clr-invert),
+					-2px -2px 0 var(--clr-blue),
+					2px -2px 0 var(--clr-blue),
+					-2px 2px 0 var(--clr-blue),
+					2px 2px 0 var(--clr-blue),
+					-2px 0 0 var(--clr-blue),
+					2px 0 0 var(--clr-blue),
+					0 -2px 0 var(--clr-blue),
+					0 2px 0 var(--clr-blue);
 				letter-spacing: 1px;
 				border-bottom: 2px solid var(--clr-blue);
 				width: fit-content;
@@ -98,10 +98,11 @@
 			}
 
 			& p {
-				color: var(--clr-gray);
+				color: var(--clr-main);
 				font-family: var(--bronova);
 				font-size: clamp(var(--sm), 1.25rem, var(--h4));
 				font-style: italic;
+				opacity: 0.5;
 				text-align: center;
 				margin-top: 2rem;
 				line-height: 1.5;
