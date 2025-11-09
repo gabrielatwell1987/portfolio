c67483ac89a4add024a1112128c3761743f5c301
gabrielatwell1987 <gabrielatwell@proton.me>
2025-11-09 11:31:22 -0800

    took the svg off of MotifPhoto

--- src/lib/components/contact/MotifPhoto.svelte
+++ src/lib/components/contact/MotifPhoto.svelte
@@ -3,7 +3,6 @@
 
 <div class="hire-wrapper">
 	<img src="/images/gabe.webp" alt="gabe atwell" />
-	<img src="/logos/hire-logo.svg" alt="hire svg" class="hire-logo" />
 </div>
 
 <style>
@@ -19,41 +18,6 @@
 			display: block;
 			margin-inline: auto;
 			z-index: 1;
-		}
-
-		& .hire-logo {
-			position: absolute;
-			bottom: 4vw;
-			right: 1vw;
-			width: 100px;
-			height: auto;
-			z-index: 5;
-			scale: 8;
-
-			@media (width <= 1400px) {
-				scale: 7;
-			}
-
-			@media (width <= 1100px) {
-				scale: 5;
-				bottom: 2.5vw;
-				right: 0;
-			}
-
-			@media (width <= 990px) {
-				right: -1vw;
-			}
-
-			@media (width <= 768px) {
-				scale: 3;
-				bottom: 0;
-				right: -3vw;
-			}
-
-			@media (width <= 500px) {
-				right: -6vw;
-				bottom: 1vw;
-			}
 		}
 	}
 </style>
