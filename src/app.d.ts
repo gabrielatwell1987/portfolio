8ee0e9774baa0faf6c2ab09c5d2c292651d4f57c
gabrielatwell1987 <gabrielatwell@proton.me>
2026-01-13 11:51:11 -0800

    had to add a declaration to app.d.ts

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,6 +1,9 @@
 declare module '*.md' {
 	const component: any;
 	export default component;
+}
+
+declare module '$env/static/private' {
 	export const GITHUB_TOKEN: string;
 	export const GITHUB_USERNAME: string;
 }
