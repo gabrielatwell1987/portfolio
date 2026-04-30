f709f848ab8616174a46fb665f896f9ad4a2ed38
gabrielatwell1987 <gabrielatwell@proton.me>
2026-04-30 13:46:37 -0700

    added a new details to greensock-details.json that's about cleanup functions

--- src/routes/learn/greensock/+page.svelte
+++ src/routes/learn/greensock/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-    import { scale } from 'svelte/transition';
     import SEO from '$lib/data/SEO.svelte';
     import Title from '$lib/components/layout/titles/Title.svelte';
     import Button from '$lib/components/layout/Button.svelte';
